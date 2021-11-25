3f6f118514917ca97b9e51f34bd55196922d81e9
yeinMOON <yeindianamoon@gmail.com>
2021-11-25 23:54:59 +0900

    feat: Add auth api(#53)

---
+++ client/src/api/auth.js
@@ -0,0 +1,32 @@
+import api from "./index";
+
+const authAPI = {
+  signup: async (username, email, password) => {
+    return await api.post(`/auth/signup`, { username, email, password });
+  },
+
+  // check email availability
+  // check username availability
+
+  signin: async (email, password) => {
+    return await api.post(`/auth/signin`, { email, password });
+  },
+
+  signout: async () => {
+    return await api.post(`/auth/logout`);
+  },
+
+  withdraw: async () => {
+    return await api.delete(`/auth/withdraw`, { data: { password } });
+  },
+
+  googleOAuth: async (googlecode) => {
+    return await api.post(`auth/oauth/google`, { googlecode });
+  },
+
+  kakaoOAuth: async (kakaocode) => {
+    return await api.post(`auth/oauth/kakao`, { kakaocode });
+  },
+};
+
+export default authAPI;
