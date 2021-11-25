710ac7dcfc9b0e641933519b5e8286e4e8dab101
Gyosic <zpzptls@naver.com>
2021-11-26 02:03:33 +0900

    feat: Add loginReducer

--- client/src/store/reducers/index.js
+++ client/src/store/reducers/index.js
@@ -4,7 +4,9 @@ import modalReducer from "./modalReducer";
 import notificationReducer from "./notificationReducer";
 
 const rootReducer = combineReducers({
+  loginReducer,
   modalReducer,
+  notificationReducer,
 });
 
 export default rootReducer;
