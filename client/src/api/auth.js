977bed4246bdc60c08fb57b35ecaf8d260042eeb
yeinMOON <yeindianamoon@gmail.com>
2021-11-27 01:26:30 +0900

    feat: Add check-availability api functions & Fix signup req params

--- client/src/api/auth.js
+++ client/src/api/auth.js
@@ -1,12 +1,20 @@
 import api from "./index";
 
 const authAPI = {
-  signup: async (username, email, password) => {
-    return await api.post(`/auth/signup`, { username, email, password });
+  signup: async (image, username, email, password) => {
+    return await api.post(`/auth/signup`, { image, username, email, password });
   },
 
-  // check email availability
-  // check username availability
+  checkAvailability: async (type, input) => {
+    let payload;
+    switch (type) {
+      case "email":
+        payload = { email: input };
+      case "username":
+        payload = { username: input };
+    }
+    return await api.post(`auth/signup/availability`, { type, ...payload });
+  },
 
   signin: async (email, password) => {
     return await api.post(`/auth/signin`, { email, password });
