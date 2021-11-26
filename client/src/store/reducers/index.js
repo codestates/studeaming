73a5e7c522651fa7e05e47b946ec057450e61387
Gyosic <zpzptls@naver.com>
2021-11-26 12:59:36 +0900

    feat: Add redux-persist

--- client/src/store/reducers/index.js
+++ client/src/store/reducers/index.js
@@ -1,7 +1,15 @@
 import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import loginReducer from "./loginReducer";
 import modalReducer from "./modalReducer";
 import notificationReducer from "./notificationReducer";
+
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["loginReducer"],
+};
 
 const rootReducer = combineReducers({
   loginReducer,
@@ -9,4 +17,4 @@ const rootReducer = combineReducers({
   notificationReducer,
 });
 
-export default rootReducer;
+export default persistReducer(persistConfig, rootReducer);
