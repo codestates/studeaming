fece42083b146d9bea70776f1694dee8c0d9296a
수민 N <suminn@sumin-ui-MacBookAir.local>
2021-11-26 17:22:23 +0900

    rename: Correct table names

--- server/migrations/20211125164402-create-currentlog.js
+++ server/migrations/20211125164402-create-currentlog.js
@@ -1,7 +1,7 @@
 "use strict";
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable("currentlogs", {
+    await queryInterface.createTable("Currentlogs", {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -23,6 +23,6 @@ module.exports = {
     });
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable("currentlogs");
+    await queryInterface.dropTable("Currentlogs");
   },
 };
