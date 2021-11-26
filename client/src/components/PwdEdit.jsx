ab83d170c726fec1f7c7e5d98a5c1beb6a820ea9
yeinMOON <yeindianamoon@gmail.com>
2021-11-27 01:31:06 +0900

    feat: Implement PwdEdit component

---
+++ client/src/components/PwdEdit.jsx
@@ -0,0 +1,143 @@
+import { useState, useEffect, useRef } from "react";
+import { useDispatch } from "react-redux";
+import styled from "styled-components";
+import userAPI from "../api/user";
+import { notify, modalOff } from "../store/actions";
+import {
+  AuthContainer,
+  Title,
+  InputContainer,
+  Desc,
+  Input,
+  ErrorMsg,
+} from "./reusableStyle";
+import Button from "./Button";
+
+const ButtonContainer = styled.div`
+  width: 220px;
+`;
+
+function PwdEdit() {
+  const currentPwInput = useRef(null);
+  const newPwInput = useRef(null);
+  const [password, setPassword] = useState({
+    current: "",
+    fresh: "",
+    check: "",
+  });
+  const [errorMessage, setErrorMessage] = useState({ fresh: "", check: "" });
+  const [isValid, setIsValid] = useState({ fresh: false, check: false });
+  const dispatch = useDispatch();
+  const regExpPwd = /^(?=.*?[a-z])(?=.*?[0-9]).{8,16}$/i;
+
+  const getPassword = (key) => (e) => {
+    setPassword({ ...password, [key]: e.target.value });
+  };
+
+  const checkValidation = () => {
+    if (regExpPwd.test(password.fresh) || !password.fresh.length) {
+      setErrorMessage({ ...errorMessage, fresh: "" });
+      setIsValid({ ...isValid, fresh: true });
+    } else {
+      setErrorMessage({
+        ...errorMessage,
+        fresh: "8~16자 영문 대 소문자, 숫자를 사용하세요.",
+      });
+      setIsValid({ ...isValid, fresh: false });
+    }
+  };
+
+  const checkMatched = () => {
+    const { fresh, check } = password;
+    if (fresh !== check && fresh.length) {
+      setErrorMessage({
+        ...errorMessage,
+        check: "비밀번호가 일치하지 않습니다.",
+      });
+      setIsValid({ ...isValid, check: false });
+    } else if (!(fresh.length && check.length)) {
+      setErrorMessage({ ...errorMessage, check: "" });
+      setIsValid({ ...isValid, check: false });
+    } else {
+      setErrorMessage({ ...errorMessage, check: "" });
+      setIsValid({ ...isValid, check: true });
+    }
+  };
+
+  const pwdEditHandler = (event) => {
+    const isRequest =
+      event.target.textContent === "회원정보 수정" ? true : false;
+    if (isRequest && !password.current.length) {
+      dispatch(notify("현재 비밀번호를 입력해주세요."));
+      currentPwInput.current.focus();
+    } else if (isRequest && isValid.fresh && isValid.check) {
+      try {
+        const encryptedCurrentPwd = crypto.AES.encrypt(
+          password.current,
+          process.env.REACT_APP_SECRET_KEY
+        ).toString();
+        const encryptedNewPwd = crypto.AES.encrypt(
+          password.fresh,
+          process.env.REACT_APP_SECRET_KEY
+        ).toString();
+        userAPI.modifyUserInfo(encryptedCurrentPwd, encryptedNewPwd);
+        dispatch(notify("비밀번호가 변경되었습니다."));
+        dispatch(modalOff());
+      } catch (err) {
+        if (err.response.status === 401) {
+          dispatch(notify("현재 비밀번호가 일치하지 않습니다."));
+          currentPwInput.current.focus();
+        }
+      }
+    } else if (isRequest) {
+      dispatch(notify("새 비밀번호를 바르게 입력해주세요."));
+      newPwInput.current.focus();
+    }
+  };
+
+  useEffect(() => {
+    checkMatched();
+    // eslint-disable-next-line
+  }, [password.check]);
+
+  return (
+    <AuthContainer>
+      <Title>비밀번호 변경</Title>
+      <InputContainer>
+        <Desc>현재 비밀번호</Desc>
+        <Input
+          type="password"
+          onChange={getPassword("current")}
+          ref={currentPwInput}
+        ></Input>
+      </InputContainer>
+      <InputContainer>
+        <Desc>새 비밀번호</Desc>
+        <Input
+          type="password"
+          onChange={getPassword("fresh")}
+          onBlur={checkValidation}
+          ref={newPwInput}
+        ></Input>
+        <ErrorMsg>{errorMessage.fresh}</ErrorMsg>
+      </InputContainer>
+      <InputContainer>
+        <Desc>새 비밀번호 확인</Desc>
+        <Input
+          type="password"
+          onChange={getPassword("check")}
+          onBlur={checkMatched}
+          onKeyUp={(e) => {
+            e.key === "Enter" && pwdEditHandler();
+          }}
+        ></Input>
+        <ErrorMsg>{errorMessage.check}</ErrorMsg>
+      </InputContainer>
+      <ButtonContainer>
+        <Button message="변경하기" clickEvent={pwdEditHandler} />
+      </ButtonContainer>
+    </AuthContainer>
+  );
+}
+
+export default PwdEdit;
