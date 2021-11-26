62dfa9a0bb9a3394f63bc7fdc4673cea1eabc2cc
수민 N <suminn@sumin-ui-MacBookAir.local>
2021-11-27 01:40:45 +0900

    feat: Add functions to use refresh token

--- server/controller/functions/tokenFunctions.js
+++ server/controller/functions/tokenFunctions.js
@@ -1,9 +1,56 @@
 const { sign, verify } = require("jsonwebtoken");
 require("dotenv").config();
+const cookieOpt = {
+  httpOnly: true,
+  //sameSite: "none",
+  //secure: true, https 사용 시 활성화
+  domain: process.env.CLIENT_DOMAIN,
+};
 
 module.exports = {
-  generateToken: (data) => {},
-  sendToken: (res, accessToken) => {},
-  isAuthorized: (req) => {},
-  clearToken: (res) => {},
+  sendAccessToken: (res, data) => {
+    const accessToken = sign(data, process.env.ACCESS_SECRET, {
+      expiresIn: "10m",
+    });
+    res.cookie("authorization", accessToken, cookieOpt);
+  },
+  sendRefreshToken: (res, data) => {
+    const refreshToken = sign(data, process.env.REFRESH_SECRET, {
+      expiresIn: "24h",
+    });
+    console.log("refresh token", refreshToken);
+    res.cookie("refresh", refreshToken, {
+      ...cookieOpt,
+      path: "/auth/refresh",
+    });
+  },
+  isAccessAuthorized: (req) => {
+    const authorization = req.cookies["authorization"];
+    if (!authorization) {
+      return null;
+    }
+    try {
+      return verify(authorization, process.env.ACCESS_SECRET);
+    } catch (err) {
+      return null;
+    }
+  },
+  isRefreshAuthorized: (req) => {
+    const refreshToken = req.cookies["refreshToken"];
+    if (!refreshToken) {
+      return null;
+    }
+    try {
+      return verify(refreshToken, process.env.REFRESH_SECRET);
+    } catch (err) {
+      return null;
+    }
+  },
+  clearToken: (res) => {
+    res.clearCookie("authorization", { domain: process.env.CLIENT_DOMAIN });
+    res.clearCookie("refresh", {
+      domain: process.env.CLIENT_DOMAIN,
+      path: "/auth/refresh",
+    });
+  },
 };
