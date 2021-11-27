e34ec966e02b4d5051b73de0165d6b9b2ddbcab2
yeinMOON <yeindianamoon@gmail.com>
2021-11-27 16:20:00 +0900

    feat: Add font for eng & Add color variables

--- client/src/styles/index.js
+++ client/src/styles/index.js
@@ -4,13 +4,52 @@ import { normalize } from "styled-normalize";
 const GlobalStyle = createGlobalStyle`
   ${normalize}
   
+  @font-face {
+    font-family: "NotoSans";
+    src: url("../assets/fonts/NotoSansKR-Regular.woff") format("woff");
+  }
+
+  @font-face {
+    font-family: "Poppins";
+    src: url("../assets/fonts/Poppins-ExtraLight.woff")format("woff");
+  }
+
   *,
   :after,
   :before {
+    font-family: "NotoSans", "Poppins", sans-serif;
     margin: 0;
     padding: 0;
     box-sizing: border-box;
     font-size: 14px;
+  }
+
+  a {
+    text-decoration: none; 
+    outline: none;
+  
+    :hover, :active {
+      text-decoration: none; 
+    }
+  }
+
+  :root {
+    --lineHeight-normal: 1;
+    --font-size-regular: 16px;
+    --font-size-small: 14px;
+    --color-main-100: #656BFF;
+    --color-main-50: #DCE6FF;
+    --color-main-25: #F6F8FF;
+    --color-destructive: #E04343;
+    --color-black: #333333;
+    --color-black-50: #5C5C60
+    --color-black-25:#8D8D8D;
+    --color-grey-bg: #f8f8f8;
+    --color-red: #FFAEAE;
+    --color-orange: #FFBE82;
+    --color-green: #B4E29E;
+    --color-blue: #A5C7E5;
+    --color-puple: #B094FE;
   }
   `;
 
