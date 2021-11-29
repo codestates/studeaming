c3549fb47753739eb7f48d9edb89db690b621391
yeinMOON <yeindianamoon@gmail.com>
2021-11-29 17:32:39 +0900

    feat: Add profile modal & Rearrange

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import { Routes, Route } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
-import { signinModalOpen } from "./store/actions/index";
 import Landing from "./pages/Landing";
 import Main from "./pages/Main";
 import Mypage from "./pages/Mypage";
@@ -12,10 +11,12 @@ import Signin from "./components/Signin";
 import Signup from "./components/Signup";
 import PwdEdit from "./components/PwdEdit";
 import UserInfoEdit from "./components/UserInfoEdit";
+import UserProfile from "./components/UserProfile";
 import StreamerSetting from "./components/StreamerSetting";
 import Header from "./components/Header";
 import NotificationCenter from "./components/NotificationCenter";
 import SideLog from "./components/SideLog";
+
 require("dotenv").config();
 
 function App() {
@@ -24,31 +25,40 @@ function App() {
     isSigninOpen,
     isPwdEditOpen,
     isUserInfoEditOpen,
+    isProfileModalOpen,
     isStreamSettingOpen,
   } = useSelector(({ modalReducer }) => modalReducer);
   const { isSideLogOpen } = useSelector(({ sideLogReducer }) => sideLogReducer);
   const isModal =
-    isSignupOpen || isSigninOpen || isUserInfoEditOpen || isStreamSettingOpen;
+    isSignupOpen ||
+    isSigninOpen ||
+    isPwdEditOpen ||
+    isUserInfoEditOpen ||
+    isProfileModalOpen.boolean ||
+    isStreamSettingOpen;
 
   return (
     <>
       <Header />
+      {isSideLogOpen && <SideLog />}
       <NotificationCenter />
-      {/* <Routes>
+      <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/main" element={<Main />} />
         <Route path="/mypage" element={<Mypage />} />
-        <Route path="/streamer" element={<Streamer />} />
-        <Route path="/viewer" element={<Viewer />} />
+        {/* <Route path="/streamer" element={<Streamer />} />
+        <Route path="/viewer" element={<Viewer />} /> */}
         <Route path="/auth" element={<Verification />} />
-      </Routes> */}
-      {isSideLogOpen && <SideLog />}
+      </Routes>
       {isModal && (
         <Modal>
           {isSignupOpen && <Signup />}
           {isSigninOpen && <Signin />}
           {isPwdEditOpen && <PwdEdit />}
           {isUserInfoEditOpen && <UserInfoEdit />}
+          {isProfileModalOpen.boolean && (
+            <UserProfile username={isProfileModalOpen.username} />
+          )}
           {isStreamSettingOpen && <StreamerSetting />}
         </Modal>
       )}
