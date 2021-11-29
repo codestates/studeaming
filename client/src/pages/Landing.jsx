284e11574bbea05c6359e7d24b3efb438fa6c7bc
Gyosic <zpzptls@naver.com>
2021-11-29 17:30:33 +0900

    feat: Add Landing.jsx for animation

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,17 +1,27 @@
 import styled from "styled-components";
+import LandingSection1 from "../components/LandingSection1";
+import LandingSection2 from "../components/LandingSection2";
+import LandingSection3 from "../components/LandingSection3";
 import Footer from "../components/Footer";
 
-const LandingContainer = styled.div`
+const LandingContainer = styled.section`
   width: 100%;
-  height: 400px;
+  height: 360vh;
+  display: flex;
+  flex-direction: column;
+  scroll-behavior: smooth;
 `;
 
 function Landing() {
   return (
-    <div>
-      <LandingContainer />
+    <>
+      <LandingContainer>
+        <LandingSection1 />
+        <LandingSection2 />
+        <LandingSection3 />
+      </LandingContainer>
       <Footer />
-    </div>
+    </>
   );
 }
 
