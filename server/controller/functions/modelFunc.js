f34653936e99cb6e7b70b54c0cb609f5dbe33492
수민 N <suminn@sumin-ui-MacBookAir.local>
2021-11-29 16:58:08 +0900

    feat: Implement controllers for managing user information

---
+++ server/controller/functions/modelFunc.js
@@ -0,0 +1,64 @@
+const { User, Studylog } = require("../../models");
+const { Op } = require("sequelize");
+
+module.exports = {
+  verifyUsername: async (username) => {
+    try {
+      const user = await User.findOne({
+        where: { username },
+      });
+
+      if (!user && username) {
+        return false;
+      } else {
+        return true;
+      }
+    } catch (err) {
+      return null;
+    }
+  },
+  verifyEmail: async (email) => {
+    try {
+      const user = await User.findOne({
+        where: { email },
+      });
+
+      if (!user && email) {
+        return false;
+      } else {
+        return true;
+      }
+    } catch (err) {
+      return null;
+    }
+  },
+  getStudyTime: async (id, start, end) => {
+    const studyLogList = await Studylog.findAll({
+      where: {
+        user_id: id,
+        finishedAt: {
+          [Op.gte]: start,
+        },
+        startedAt: {
+          [Op.lte]: end,
+        },
+      },
+      raw: true,
+    });
+    //start 에서 end까지 studylog들을 찾고
+    const time = studyLogList.reduce((acc, currentLog) => {
+      if (currentLog.startedAt < start) {
+        currentLog.startedAt = start;
+      }
+      if (currentLog.finishedAt > end || currentLog.finishedAt === null) {
+        currentLog.finishedAt = end;
+      }
+      return (
+        acc + currentLog.finishedAt.getTime() - currentLog.startedAt.getTime()
+      );
+    }, 0);
+
+    return time;
+    //각 배열 요소들의 startedAt(start보다 전이라면 start)부터 finishedAt(null이라면 현재시간, end보다 뒤라면 end)까지를 더한다
+  },
+};
--- server/controller/user.js
+++ server/controller/user.js
@@ -1,7 +1,176 @@
+const { Op } = require("sequelize");
+const { User, Currentlog, user_follower } = require("../models");
+const { isAccessAuthorized } = require("./functions/tokenFunc");
+const { verifyUsername, getStudyTime } = require("./functions/modelFunc");
+const { encrypt, decrypt } = require("./functions/encryptFunc");
+
 module.exports = {
-  getUser: (req, res) => {},
-  getProfile: (req, res) => {},
-  getFollows: (req, res) => {},
-  followUser: (req, res) => {},
-  deleteFollow: (req, res) => {},
+  getUser: async (req, res) => {
+    try {
+      const id = isAccessAuthorized(req).id;
+
+      const user = await User.findOne({
+        where: { id },
+        attributes: [
+          "id",
+          "username",
+          "email",
+          "profileImg",
+          "about",
+          "studeaming",
+        ],
+        raw: true,
+      });
+
+      if (user) {
+        res.send({ user: user });
+      } else {
+        res.status(401).send({ message: "Invalid access token" });
+      }
+    } catch (e) {
+      res.status(500).send(e);
+    }
+  },
+
+  editUser: async (req, res) => {
+    try {
+      const id = isAccessAuthorized(req).id;
+
+      if (!req.body.username || verifyUsername(req.body.username)) {
+        const user = await User.findOne({ where: { id }, raw: true });
+
+        const profileImg = req.body.profileImg || user.profileImg;
+        const username = req.body.username || user.username;
+        const about = req.body.about || user.about;
+        console.log(profileImg, username, about);
+        await User.update({ profileImg, username, about }, { where: { id } });
+
+        res.status(200).send({
+          user: {
+            id: user.id,
+            email: user.email,
+            username: username,
+            profileImg: profileImg,
+            about: about,
+            studeaming: user.studeaming,
+          },
+          message: "Modified",
+        });
+      } else {
+        res.status(409).send({ message: "Username already exists" });
+      }
+    } catch (e) {
+      res.status(500).send(e);
+    }
+  },
+
+  editPassword: async (req, res) => {
+    try {
+      const id = isAccessAuthorized(req).id;
+      const { currentPassword, newPassword } = req.body;
+      const user = await User.findOne({ where: { id }, raw: true });
+
+      decrypt(res, currentPassword, user.password, () => {
+        encrypt(res, newPassword, async (hash) => {
+          await User.update({ password: hash }, { where: { id } });
+          res.send({ message: "Password updated" }); //todo:gitbook
+        });
+      });
+    } catch (e) {
+      res.status(500).send(e);
+    }
+  },
+
+  getProfile: async (req, res) => {
+    const username = req.params.username;
+
+    try {
+      const user = await User.findOne({
+        where: { username },
+        attributes: ["id", "username", "profileImg", "about"],
+        raw: true,
+      });
+
+      const currentlogs = await Currentlog.findAll({
+        where: { user_id: user.id, name: { [Op.ne]: "휴식" } },
+        attributes: ["name"],
+        raw: true,
+      });
+
+      const now = new Date();
+      const aMonthAgo = new Date(new Date().setDate(now.getDate() - 30));
+
+      const studyTime = await getStudyTime(user.id, aMonthAgo, now);
+
+      res.send({
+        profile: {
+          ...user,
+          studylogList: currentlogs.map((obj) => obj.name),
+          studyTime: studyTime,
+        },
+      });
+    } catch (e) {
+      res.status(500).send(e);
+    }
+  },
+
+  getFollows: async (req, res) => {
+    try {
+      const user_id = isAccessAuthorized(req).id;
+
+      const studeamerIdList = await user_follower.findAll({
+        where: { user_id },
+        raw: true,
+      });
+      console.log(studeamerIdList);
+
+      const studeamerList = await User.findAll({
+        where: {
+          id: studeamerIdList.map((obj) => obj.studeamer_id), //todo: 빈 배열일 때 오류 없이 반환되는지 테스트
+        },
+        attributes: ["username", "profileImg"],
+        raw: true,
+      });
+
+      res.send({ studeamerList: studeamerList });
+    } catch (e) {
+      res.status(500).send(e);
+    }
+  },
+
+  followUser: async (req, res) => {
+    try {
+      const user = isAccessAuthorized(req);
+      const username = req.params.username;
+
+      const studeamer = await User.findOne({
+        where: { username },
+        attributes: ["id", "username", "profileImg"],
+        raw: true,
+      });
+      console.log(studeamer);
+      await user_follower.findOrCreate({
+        where: { user_id: user.id, studeamer_id: studeamer.id },
+      });
+
+      res.send({ newFollow: studeamer });
+    } catch (e) {
+      res.status(500).send(e);
+    }
+  },
+
+  deleteFollow: async (req, res) => {
+    try {
+      const user_id = isAccessAuthorized(req).id;
+      const username = req.params.username;
+      const studeamer = await User.findOne({ where: { username }, raw: true });
+      const studeamer_id = studeamer.id;
+
+      await user_follower.destroy({ where: { user_id, studeamer_id } });
+
+      res.sendStatus(204);
+    } catch (e) {
+      res.status(500).send(e);
+    }
+  },
 };
