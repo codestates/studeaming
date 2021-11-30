01feca664705e4f0e5d7bcf045d6f96f5295e333
Soomin Na <soominnna@gmail.com>
2021-12-01 00:04:16 +0900

    refactor: Move API call in oauth controller to functions directory for seperation

---
+++ server/controller/functions/OAuthFunc.js
@@ -0,0 +1,80 @@
+const axios = require("axios");
+require("dotenv").config();
+
+module.exports = {
+  getGoogleToken: async (code) => {
+    try {
+      const response = await axios({
+        method: "get",
+        url: "https://oauth2.googleapis.com/token",
+        params: {
+          code: code,
+          client_id: process.env.GOOGLE_CLIENT_ID,
+          client_secret: process.env.GOOGLE_CLIENT_SECRET,
+          redirect_uri: process.env.CLIENT_URL,
+          grant_type: "authorization_code",
+        },
+      });
+
+      return response.data.access_token;
+    } catch (e) {
+      return null;
+    }
+  },
+
+  getKakaoToken: async (code) => {
+    try {
+      const response = await axios({
+        method: "post",
+        url: "https://kauth.kakao.com/oauth/token",
+        params: {
+          code: code,
+          client_id: process.env.KAKAO_CLIENT_ID,
+          redirect_uri: process.env.CLIENT_URL,
+          grant_type: "authorization_code",
+        },
+      });
+
+      return response.data.access_token;
+    } catch (e) {
+      return null;
+    }
+  },
+
+  getGoogleEmail: async (accessToken) => {
+    try {
+      const data = await axios({
+        method: "get",
+        url: "https://www.googleapis.com/oauth2/v2/userinfo.email",
+        headers: {
+          authorization: `token ${accessToken}`,
+          accept: "application/json",
+        },
+      });
+
+      return data.data;
+    } catch {
+      return null;
+    }
+  },
+
+  getKakaoEmail: async (accessToken) => {
+    try {
+      const data = await axios({
+        method: "post",
+        url: "https://kauth.kakao.com/v2/user/me",
+        headers: {
+          authorization: `Bearer ${accessToken}`,
+          accept: "application/x-www-form-urlencoded;charset=utf-8",
+        },
+        //params: {property_keys=["kakao_account.email"]}
+      });
+
+      return data.data;
+    } catch (e) {
+      return null;
+    }
+  },
+
+  disconnect: (id) => {},
+};
