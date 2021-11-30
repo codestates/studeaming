7c7ca6707a60eb4fde05394d2a2659ff1e76d6b5
yeinMOON <yeindianamoon@gmail.com>
2021-11-30 13:48:10 +0900

    feat: Build structure between sidebar and calendarr

--- client/src/pages/Mypage.jsx
+++ client/src/pages/Mypage.jsx
@@ -4,12 +4,30 @@ import Sidebar from "../components/Sidebar";
 const Container = styled.div`
   display: flex;
   justify-content: flex-start;
+
+  @media screen and (max-width: 768px) {
+    justify-content: flex-end;
+    flex-direction: column;
+  }
+`;
+
+const Calendar_temp = styled.div`
+  width: calc(100vw - 300px);
+  height: calc(100vh - 61.69px);
+  background-color: lightgreen;
+
+  @media screen and (max-width: 768px) {
+    width: 100vw;
+    min-width: 400px;
+    order: -1;
+  }
 `;
 
 function Mypage() {
   return (
     <Container>
       <Sidebar />
+      <Calendar_temp />
     </Container>
   );
 }
