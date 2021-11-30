90d9becf0dc29456a0527d6e2af1a389db918e55
yeinMOON <yeindianamoon@gmail.com>
2021-12-01 02:36:37 +0900

    feat: Add withdrawal success modal

--- client/src/components/Withdrawal.jsx
+++ client/src/components/Withdrawal.jsx
@@ -1,7 +1,11 @@
 import { useState } from "react";
 import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
+import { BsCheckCircle } from "react-icons/bs";
+import crypto from "crypto-js";
 import authAPI from "../api/auth";
+import { modalOff } from "../store/actions";
 import Button from "./Button";
 import { Title, Input } from "./reusableStyle";
 
@@ -10,11 +14,24 @@ const Container = styled.div`
   flex-direction: column;
   align-items: center;
 
-  #withdrawal_description {
+  .withdrawal_description {
+    color: var(--color-black-50);
     font-size: 0.8rem;
     text-align: center;
+    line-height: 1.2rem;
     margin-bottom: 1rem;
   }
+
+  #withdrawal_success_container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  #withdrawal_success {
+    margin: 0.8rem 0;
+  }
+
   #warning_message {
     color: var(--color-destructive);
     font-size: 0.7rem;
@@ -22,37 +39,72 @@ const Container = styled.div`
   }
 `;
 
+const StyledCheck = styled(BsCheckCircle)`
+  color: var(--color-main-100);
+  margin-bottom: 5px;
+`;
+
 function Withdrawal() {
   const { isSocialLogined } = useSelector(({ userReducer }) => userReducer);
+  const [password, setPassword] = useState("");
   const [isSuccess, setIsSuccess] = useState(false);
+  const navigate = useNavigate();
+
+  const getPassword = (e) => {
+    setPassword(e.target.value);
+  };
 
   const withdrawalHandler = () => {
-    // 탈퇴에 성공하면 isSuccess 상태 true 및 main페이지로 이동
-    // true일때 탈퇴 완료 모달로 변경
-    // 완료 확인 버튼 누르면 modalOff()
+    const encryptedPwd = crypto.AES.encrypt(
+      password,
+      process.env.REACT_APP_SECRET_KEY
+    ).toString();
+
+    authAPI
+      .withdraw(encryptedPwd)
+      .then(() => {
+        setIsSuccess(true);
+        navigate("/main");
+      })
+      .catch(() => {});
   };
 
   return (
     <Container>
       <Title>탈퇴하기</Title>
-      <div id="withdrawal_description">
-        스터디밍 서비스를 더 이상 사용하지 않으신다면
-        <br />
-        {isSocialLogined ? "아래 메세지" : "비밀번호"}를 입력한 후 탈퇴할 수
-        있습니다.
-      </div>
-      <Input
-        type="password"
-        placeholder={
-          isSocialLogined
-            ? `'잘있어요 스터디밍'을 입력해주세요`
-            : "비밀번호를 입력해주세요"
-        }
-      />
-      <div id="warning_message">
-        탈퇴 후에는 사용자 정보가 즉시 삭제되며 복구할 수 없습니다.
-      </div>
-      <Button message="탈퇴 완료" clickEvent={withdrawalHandler} />
+      {isSuccess ? (
+        <div id="withdrawal_success_container">
+          <StyledCheck size="30px" />
+          <div id="withdrawal_success">탈퇴가 완료되었습니다.</div>
+          <div className="withdrawal_description">
+            스터디밍은 언제나 당신의 배움을 응원하겠습니다.
+            <br />
+            이용해주셔서 감사합니다.
+          </div>
+        </div>
+      ) : (
+        <div>
+          <div className="withdrawal_description">
+            스터디밍 서비스를 더 이상 사용하지 않으신다면
+            <br />
+            {isSocialLogined ? "아래 메세지" : "비밀번호"}를 입력한 후 탈퇴할 수
+            있습니다.
+          </div>
+          <Input
+            type="password"
+            onChange={getPassword}
+            placeholder={
+              isSocialLogined
+                ? `'잘있어요 스터디밍'을 입력해주세요`
+                : "비밀번호를 입력해주세요"
+            }
+          />
+          <div id="warning_message">
+            탈퇴 후에는 사용자 정보가 즉시 삭제되며 복구할 수 없습니다.
+          </div>
+          <Button message="탈퇴 완료" clickEvent={withdrawalHandler} />{" "}
+        </div>
+      )}
     </Container>
   );
 }
