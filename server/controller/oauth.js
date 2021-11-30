0c946ee24b11f391be5074c03dcad6513c60d3d3
Soomin Na <soominnna@gmail.com>
2021-12-01 00:04:16 +0900

    refactor: Move API call in oauth controller to functions directory for seperation

--- server/controller/oauth.js
+++ server/controller/oauth.js
@@ -1,83 +1,51 @@
 const { User } = require("../models");
-const axios = require("axios");
 const { sendAccessToken, sendRefreshToken } = require("./functions/tokenFunc");
 const generateName = require("./functions/generateUsername");
-require("dotenv").config();
+const {
+  getKakaoToken,
+  getGoogleToken,
+  getGoogleEmail,
+  getKakaoEmail,
+} = require("./functions/OAuthFunc");
 
 module.exports = {
   googleSignin: async (req, res) => {
     try {
       //todo:인증 실패 시 응답
-      const response = await axios({
-        method: "get",
-        url: "https://oauth2.googleapis.com/token",
-        params: {
-          code: req.body.code,
-          client_id: process.env.GOOGLE_CLIENT_ID,
-          client_secret: process.env.GOOGLE_CLIENT_SECRET,
-          redirect_uri: process.env.CLIENT_URL,
-          grant_type: "authorization_code",
-        },
-      });
-
-      const accessToken = response.data.access_token; // todo:응답 객체 확인
-
-      const data = await axios({
-        method: "get",
-        url: "https://www.googleapis.com/oauth2/v2/userinfo.email",
-        headers: {
-          authorization: `token ${accessToken}`,
-          accept: "application/json",
-        },
-      });
+      const accessToken = await getGoogleToken(req.body.code); // todo:응답 객체 확인
+      const data = await getGoogleEmail(accessToken);
 
       //todo:응답 객체 확인 email, name
       //todo:구글 인증 오류 처리
-      const email = data.data.email;
-      const username = generateName(data.data.name);
+      if (data) {
+        const email = data.email;
+        const username = generateName(data.name);
 
-      const [user, created] = await User.findOrCreate({
-        where: { email: email, platformType: "google" },
-        defaults: { username: username, isEmailVerified: true },
-        raw: true,
-      });
+        const [user, created] = await User.findOrCreate({
+          where: { email: email, platformType: "google" },
+          defaults: { username: username, isEmailVerified: true },
+          raw: true,
+        });
 
-      sendAccessToken(res, { id: user.id });
-      sendRefreshToken(res, { id: user.id });
+        sendAccessToken(res, { id: user.id });
+        sendRefreshToken(res, { id: user.id });
+      } else {
+        res.sendStatus(500);
+      }
     } catch {
       res.sendStatus(500);
     }
   },
   kakaoSignin: async (req, res) => {
     try {
-      const response = await axios({
-        method: "post",
-        url: "https://kauth.kakao.com/oauth/token",
-        params: {
-          code: req.body.code,
-          client_id: process.env.KAKAO_CLIENT_ID,
-          redirect_uri: process.env.CLIENT_URL,
-          grant_type: "authorization_code",
-        },
-      });
+      const accessToken = await getKakaoToken(req.body.code); // todo:응답 객체 확인
+      const data = await getKakaoEmail(accessToken);
 
-      const accessToken = response.data.access_token;
+      if (data) {
+        const email = data.kakao_account.email;
+        const { nickname, profile_image_url } = data.kakao_account.profile;
+        const username = generateName(nickname);
 
-      const data = await axios({
-        method: "post",
-        url: "https://kauth.kakao.com/v2/user/me",
-        headers: {
-          authorization: `Bearer ${accessToken}`,
-          accept: "application/x-www-form-urlencoded;charset=utf-8",
-        },
-        //params: {property_keys=["kakao_account.email"]}
-      });
-
-      const email = data.data.kakao_account.email;
-      const { nickname, profile_image_url } = data.data.kakao_account.profile;
-      const username = generateName(nickname);
-
-      if (email) {
         const [user, created] = await User.findOrCreate({
           where: { email: email, platformType: "kakao" },
           defaults: {
@@ -91,7 +59,7 @@ module.exports = {
         sendAccessToken(res, { id: user.id });
         sendRefreshToken(res, { id: user.id });
       } else {
-        //todo:사용자가 이메일 정보를 허용하지 않음
+        res.sendStatus(500);
       }
     } catch {
       res.sendStatus(500);
