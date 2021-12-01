6c109bf492367a67438e9f66fc79be65e3eaa69a
yeinMOON <yeindianamoon@gmail.com>
2021-12-02 01:12:22 +0900

    feat: Add Calendar component

---
+++ client/src/components/Calendar.jsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 100%;
+  height: 84%;
+  background-color: lightgray;
+`;
+
+function Calendar() {
+  return (
+    <Container>
+      <div></div>
+    </Container>
+  );
+}
+
+export default Calendar;
