032c2bf2045b13a2adb224e6a7a3ff6f42f0aae9
Soomin Na <soominnna@gmail.com>
2021-12-03 23:53:16 +0900

    feat: Implement withdraw for Google and Kakao OAuth user

--- server/controller/auth/withdraw.js
+++ server/controller/auth/withdraw.js
@@ -2,18 +2,46 @@ const { User } = require("../../models");
 const encryptPassword = require("../functions/encryption");
 const { clearToken, isAccessAuthorized } = require("../functions/token");
 const { dropData } = require("../functions/model");
+const { disconnectKakao } = require("../functions/OAuth");
 
 module.exports = async (req, res) => {
   try {
     const id = isAccessAuthorized(req).id;
 
-    const user = await User.findByPk(id);
+    const user = await User.findByPk(id, { raw: true });
 
-    encryptPassword.decrypt(res, req.body.password, user.password, async () => {
-      await dropData(user.id);
-      clearToken(res);
-      res.sendStatus(204);
-    });
+    switch (user.platformType) {
+      case "google":
+        await dropData(user.id);
+        clearToken(res);
+        res.sendStatus(204);
+
+        break;
+
+      case "kakao":
+        const kakaoId = await disconnectKakao(user.authCode);
+        if (kakaoId) {
+          await dropData(user.id);
+          clearToken(res);
+          res.sendStatus(204);
+        } else {
+          res.sendStatus(500);
+        }
+
+        break;
+
+      default:
+        encryptPassword.decrypt(
+          res,
+          req.body.password,
+          user.password,
+          async () => {
+            await dropData(user.id);
+            clearToken(res);
+            res.sendStatus(204);
+          }
+        );
+    }
   } catch (e) {
     res.status(500).send(e);
   }
