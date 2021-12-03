22d9ba9831342b5f29905eb86a093f22ce3c396e
yeinMOON <yeindianamoon@gmail.com>
2021-12-03 23:22:57 +0900

    feat: Add comment CRUD

--- client/src/components/DailyLog.jsx
+++ client/src/components/DailyLog.jsx
@@ -7,13 +7,14 @@ import LogChart from "./LogChart";
 const Container = styled.div`
   display: flex;
   padding: 0.4rem;
-  gap: 1.2rem;
+  gap: 1.4rem;
+
+  @media screen and (max-width: 768px) {
+    flex-direction: column;
+  }
 
   #date {
     font-size: 1.4rem;
-  }
-
-  #day {
   }
 
   #right {
@@ -25,56 +26,137 @@ const Container = styled.div`
   .comment-header {
     display: flex;
     justify-content: space-between;
-    margin: 1rem 0 0.4rem 0;
-  }
+    align-items: center;
+    margin: 1.4rem 0 0.8rem 0;
+    padding: 0 0.2rem;
 
-  .comment-box {
-    background-color: var(--color-main-25);
-    border-radius: 0.8rem;
-    padding: 1rem;
+    .comment-header-title {
+      color: var(--color-black-50);
+    }
+
+    .comment-header-delete {
+      font-size: 0.8rem;
+      color: var(--color-black-50);
+
+      :hover {
+        font-weight: 700;
+      }
+    }
+
+    .comment-header-delete-check {
+      font-size: 0.8rem;
+      color: var(--color-black-50);
+      margin-right: 0.4rem;
+    }
+
+    .removal-button {
+      padding: 0.2rem 0.4rem;
+      :hover {
+        background-color: var(--color-main-25);
+        color: var(--color-main-100);
+        border-radius: 0.6rem;
+      }
+    }
   }
 
   .log-header {
     display: flex;
     flex-direction: column;
     padding: 0 0 0.4rem;
-    margin-bottom: 0.4rem;
+    margin-bottom: 0.6rem;
 
-    .studytime-title {
-      font-size: 0.9rem;
-      color: var(--color-black-25);
+    @media screen and (max-width: 768px) {
+      display: none;
     }
+  }
 
-    .studytime {
-      font-size: 2rem;
-      font-weight: 700;
-      color: var(--color-black-25);
-    }
+  .studytime-title {
+    font-size: 0.9rem;
+    color: var(--color-black-25);
+  }
+
+  .studytime {
+    font-size: 2rem;
+    font-weight: 700;
+    color: var(--color-black-25);
   }
 
   .chartbox {
     width: 180px;
-    height: 290px;
+    height: 330px;
+
+    @media screen and (max-width: 768px) {
+      width: 100%;
+      padding-right: 1rem;
+    }
+  }
+`;
+
+const CommentBox = styled.div`
+  background-color: ${(props) =>
+    props.isEditing ? "transparent" : "var(--color-main-25)"};
+  border: 2px solid
+    ${(props) => (props.isEditing ? "var(--color-main-50)" : "transparent")};
+  border-radius: 0.8rem;
+  padding: 1rem;
+
+  @media screen and (max-width: 768px) {
+    height: 100px;
+  }
+
+  textarea {
+    font-family: "NotoSans" !important;
+    font-size: 0.8rem;
   }
 `;
 
 function DailyLog({ moment }) {
   const [studyTime, setStudyTime] = useState({ hour: 9, minute: 52 });
+  const [isEditing, setIsEditing] = useState(false);
+  const [comment, setComment] = useState("");
+  const [isForSure, setIsForSure] = useState(false);
   const date = moment.format("YYYYMMDD");
   const offset = new window.Date().getTimezoneOffset();
 
   const getDailyData = async (date, offset) => {
     try {
-      const res = await logAPI.getLogs(date, offset);
-      const hour = Math.floor(res.data.studyTime / 60);
-      const minute = res.data.studyTime % 60;
+      const logRes = await logAPI.getLogs(date, offset);
+      const commentRes = await logAPI.getComment(date);
+      const hour = Math.floor(logRes.data.studyTime / 60);
+      const minute = logRes.data.studyTime % 60;
       // setStudyTime({hour, minute});
+      // setComment(commentRes.data.comment);
     } catch {}
+  };
+
+  const getComment = (e) => {
+    setComment(e.target.value);
+  };
+
+  const finishEditingHandler = () => {
+    setIsEditing(false);
+    logAPI.modifyComment(date, comment);
+  };
+
+  const checkRemoval = () => {
+    setIsForSure(true);
+  };
+
+  const removeHandler = () => {
+    setIsEditing(false);
+    setComment("");
+    logAPI
+      .modifyComment(date, comment)
+      .then(() => {
+        setIsForSure(false);
+      })
+      .catch(() => {});
+    setIsForSure(false); /* 서버 요청 후 삭제 */
   };
 
   useEffect(() => {
     getDailyData(date, offset);
-  });
+  }, []);
 
   return (
     <Container>
@@ -83,12 +165,39 @@ function DailyLog({ moment }) {
         <span id="day">{moment.locale("ko").format("dddd")}</span>
         <section className="comment-area">
           <div className="comment-header">
-            <span>오늘의 코멘트</span>
-            <button>전체 삭제</button>
+            <span className="comment-header-title">오늘의 코멘트</span>
+            {isForSure ? (
+              <div>
+                <span className="comment-header-delete-check">
+                  정말 모두 지우시겠어요?
+                </span>
+                <button className="removal-button" onClick={removeHandler}>
+                  YES
+                </button>
+                <button
+                  className="removal-button"
+                  onClick={() => setIsForSure()}
+                >
+                  NO
+                </button>
+              </div>
+            ) : (
+              <button className="comment-header-delete" onClick={checkRemoval}>
+                전체 삭제
+              </button>
+            )}
           </div>
-          <div className="comment-box">
-            <textarea rows="13" cols="35"></textarea>
-          </div>
+          <CommentBox isEditing={isEditing}>
+            <textarea
+              onFocus={() => setIsEditing(true)}
+              onBlur={finishEditingHandler}
+              onChange={getComment}
+              value={comment}
+              rows="18"
+              cols="50"
+              spellcheck="false"
+            ></textarea>
+          </CommentBox>
         </section>
       </div>
       <div id="right">
