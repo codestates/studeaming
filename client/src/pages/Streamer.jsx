cf0e769c333beffac7ead42ba0718069b54ea615
Deb-neal <dksaksen7@gmail.com>
2021-12-04 02:05:09 +0900

    Add: model socketio data

--- client/src/pages/Streamer.jsx
+++ client/src/pages/Streamer.jsx
@@ -4,7 +4,7 @@ import StreamerSetting from "../components/StreamerSetting";
 function Streamer() {
   //todo: 여기서는 StreamerSetting에서 만들어진 데이터만 가져와야지
   //todo: 그럼 일단 저 StreamerSetting에서 만들어진 장치아이디, 유유아이디, 기타등등
-  return;
+  return <StreamerSetting />;
 }
 
 export default Streamer;
--- server/controller/studyRoom/index.js
+++ server/controller/studyRoom/index.js
@@ -16,8 +16,7 @@ module.exports = {
     //     console.log(data.room);
     //   })
     // );
-    socket.on("join_room", (data) => {
-      console.log("유유아이디", data.uuid);
+    socket.on("join_room", async (data) => {
       if (users[data.room]) {
         //todo: 방이 이미 존재할때
         const length = users[data.room].length;
@@ -25,23 +24,23 @@ module.exports = {
           socket.to(socket.id).emit("room_full");
           return;
         }
-        console.log("방이 존재할때", users[data.room]);
         users[data.room].push({ id: socket.id, email: data.email });
       } else {
         //todo: 방이 존재하지 않을때
-        const socketData = socketio_data.findOrCreate({
+        console.log("방제목", data.room);
+        const [socketData, created] = await socketio_data.findOrCreate({
           where: { uuid: data.uuid },
           defaults: {
             studeamer_id: socket.id,
           },
         });
+        //todo: 새로 만들어지는 상황이 아니라면 그냥 return
+        if (!created) return;
         users[data.room] = [
           { uuid: newID(), id: socket.id, email: data.email },
         ];
-        console.log("방이 존재하지않을때 방정보", users[data.room]);
       }
       socketToRoom[socket.id] = data.room;
-      console.log("소켓투룸", socketToRoom[socket.id]);
       socket.join(data.room);
       console.log(
         "유저님이 입장하셨습니다.",
