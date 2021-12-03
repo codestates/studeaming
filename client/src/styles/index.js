bc5d62165997b2c6b83fa169c211eee4119f885b
yeinMOON <yeindianamoon@gmail.com>
2021-12-03 09:40:15 +0900

    design: Add global color & Fix font def

--- client/src/styles/index.js
+++ client/src/styles/index.js
@@ -4,7 +4,6 @@ import NotoSans from "../assets/fonts/NotoSansKR-Regular.woff";
 import Poppins_EL from "../assets/fonts/Poppins-ExtraLight.woff";
 import Poppins_M from "../assets/fonts/Poppins-Medium.woff";
 import Poppins_R from "../assets/fonts/Poppins-Regular.woff";
-import OpenSans from "../assets/fonts/OpenSans-VariableFont.woff";
 
 const GlobalStyle = createGlobalStyle`
   ${normalize}
@@ -12,6 +11,7 @@ const GlobalStyle = createGlobalStyle`
   @font-face {
     font-family: "NotoSans";
     src: local("NotoSans"), url(${NotoSans}) format("woff");
+    unicode-range: U+AC00-D7A3, U+0030-0039
   }
 
   @font-face {
@@ -32,20 +32,15 @@ const GlobalStyle = createGlobalStyle`
     unicode-range: U+0041-005A, U+0061-007A
   }
 
-  @font-face {
-    font-family: "OpenSans";
-    src: local("OpenSans"), url(${OpenSans}) format("woff");
-    unicode-range: U+0030-0039;
-  }
-
   *,
   :after,
   :before {
-    font-family: "NotoSans", "Poppins-Medium", "OpenSans", sans-serif;
+    font-family: "NotoSans", "Poppins-Medium", "Poppins-Extra_Light", "Poppins-Regular", sans-serif;
     margin: 0;
     padding: 0;
     box-sizing: border-box;
     font-size: var(--font-size-regular);
+
     @media screen and (max-width: 768px) {
       font-size: var(--font-size-small);
     }    
@@ -68,13 +63,16 @@ const GlobalStyle = createGlobalStyle`
     --font-size-regular: 16px;
     --font-size-small: 14px;
     --color-main-100: #656BFF;
-    --color-main-50: #DCE6FF;
-    --color-main-25: #F6F8FF;
+    --color-main-75: #9296FF;
+    --color-main-50: #CFD1FF;
+    --color-main-25: #E3E4FF;
+    --color-main-0: #F6F8FF;
     --color-destructive: #E04343;
     --color-black: #333333;
     --color-black-50: #5C5C60;
     --color-black-25:#8D8D8D;
     --color-gray-bg: #f8f8f8;
+    --color-gray-bg-50: #FDFDFD;
     --color-red: #FFAEAE;
     --color-pink: #FBB4EB;
     --color-orange: #FFBE82;
