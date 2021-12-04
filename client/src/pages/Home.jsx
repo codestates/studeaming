2360022a37d58d3da7953e1c2a552404fe3927d8
Gyosic <zpzptls@naver.com>
2021-12-04 12:09:41 +0900

    feat: Add search feat, filter feat,dummy data

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import styled from "styled-components";
 import { AiOutlineSearch } from "react-icons/ai";
+import Fuse from "fuse.js";
 import { verifySocialLogined, getUserInfo, getFollows } from "../store/actions";
 import Slider from "../components/Slider";
 import MainContents from "../components/MainContents";
@@ -10,6 +11,10 @@ import Loading from "../components/Loading";
 import authAPI from "../api/auth";
 import userAPI from "../api/user";
 import empty from "../assets/images/empty.png";
+import thumbnail1 from "../assets/images/thumbnail1.png";
+import thumbnail2 from "../assets/images/thumbnail2.png";
+import thumbnail3 from "../assets/images/thumbnail3.png";
+import thumbnail4 from "../assets/images/thumbnail4.png";
 
 const SearchSection = styled.section`
   display: flex;
@@ -67,8 +72,109 @@ const NotContents = styled.section`
 `;
 
 function Home() {
-  const [searchValue, SetSearchValue] = useState("");
-  const [contents, setContents] = useState([""]);
+  const [searchItems, setSearchItems] = useState([
+    {
+      title: "공무원 시험준비 D-182",
+      thumbnail: thumbnail1,
+      profileImg: "",
+      username: "고시생",
+      viewer: 1,
+      openedAt: 12345671,
+    },
+    {
+      title: "전기기사 시험 D-22 / 화이팅",
+      thumbnail: thumbnail2,
+      profileImg: "",
+      username: "전기전자",
+      viewer: 2,
+      openedAt: 12345678,
+    },
+    {
+      title: "기말고사 준비 / 집중이 안돼요",
+      thumbnail: thumbnail3,
+      profileImg: "",
+      username: "21학번 김코딩",
+      viewer: 0,
+      openedAt: 12345673,
+    },
+    {
+      title: "딸래미 시험 D-2 / 같이 공부해요",
+      thumbnail: thumbnail4,
+      profileImg: "",
+      username: "공부왕 찐천재 홍진경",
+      viewer: 3,
+      openedAt: 12345672,
+    },
+    {
+      title:
+        "딸래미 시험 D-2 title이 길면 어떻게 되는지 보고싶어서 일부러 길게 쓴거임 / 같이 공부해요",
+      thumbnail: thumbnail4,
+      profileImg: "",
+      username: "공부왕 찐천재 홍진경",
+      viewer: 3,
+      openedAt: 12345672,
+    },
+    {
+      title: "토익 900점을 위해 / 스터디 윗미 3일차",
+      thumbnail: thumbnail1,
+      profileImg: "",
+      username: "박해커",
+      viewer: 2,
+      openedAt: 12645672,
+    },
+  ]);
+  const [contents, setContents] = useState([
+    {
+      title: "공무원 시험준비 D-182",
+      thumbnail: thumbnail1,
+      profileImg: "",
+      username: "고시생",
+      viewer: 1,
+      openedAt: 12345671,
+    },
+    {
+      title: "전기기사 시험 D-22 / 화이팅",
+      thumbnail: thumbnail2,
+      profileImg: "",
+      username: "전기전자",
+      viewer: 2,
+      openedAt: 12345678,
+    },
+    {
+      title: "기말고사 준비 / 집중이 안돼요",
+      thumbnail: thumbnail3,
+      profileImg: "",
+      username: "21학번 김코딩",
+      viewer: 0,
+      openedAt: 12345673,
+    },
+    {
+      title: "딸래미 시험 D-2 / 같이 공부해요",
+      thumbnail: thumbnail4,
+      profileImg: "",
+      username: "공부왕 찐천재 홍진경",
+      viewer: 3,
+      openedAt: 12345672,
+    },
+    {
+      title:
+        "딸래미 시험 D-2 딸래미 시험 D-2 title이 길면 어떻게 되는지 보고싶어서 일부러 길게 쓴거임 / 같이 공부해요",
+      thumbnail: thumbnail4,
+      profileImg: "",
+      username: "공부왕 찐천재 홍진경",
+      viewer: 3,
+      openedAt: 12345672,
+    },
+    {
+      title: "토익 900점을 위해 / 스터디 윗미 3일차",
+      thumbnail: thumbnail1,
+      profileImg: "",
+      username: "박해커",
+      viewer: 2,
+      openedAt: 12645672,
+    },
+  ]);
+  const [selectedFilterOpt, setSelectedFilterOpt] = useState("시청자 순");
   const [isLoading, setIsLoading] = useState(true);
   const dispatch = useDispatch();
   const filterOpt = ["시청자 순", "최신 순", "오래 공부한 순"];
@@ -114,6 +220,36 @@ function Home() {
       .catch((err) => console.log(err));
   };
 
+  const searchItem = (query) => {
+    const fuse = new Fuse(contents, {
+      keys: ["title", "username"],
+    });
+
+    const result = fuse.search(query);
+    const finalResult = [];
+    if (result.length) {
+      result.forEach((item) => {
+        finalResult.push(item.item);
+      });
+      filterHandler(finalResult);
+    } else {
+      filterHandler(contents);
+    }
+  };
+
+  const filterHandler = (items) => {
+    if (selectedFilterOpt === "시청자 순") {
+      const sort = [...items].sort((a, b) => b.viewer - a.viewer);
+      setSearchItems(sort);
+    } else if (selectedFilterOpt === "최신 순") {
+      const sort = [...items].sort((a, b) => b.openedAt - a.openedAt);
+      setSearchItems(sort);
+    } else if (selectedFilterOpt === "오래 공부한 순") {
+      const sort = [...items].sort((a, b) => a.openedAt - b.openedAt);
+      setSearchItems(sort);
+    }
+  };
+
   useEffect(() => {
     if (code) {
       if (state === "kakao") tryKakaoOAuth(code);
@@ -128,41 +264,48 @@ function Home() {
     }, 1000);
   }, []);
 
+  useEffect(() => {
+    filterHandler(searchItems);
+  }, [selectedFilterOpt]);
+
   return (
     <>
-      <section id="main">
-        <Slider />
-        <SearchSection>
-          <div className="input-box">
-            <SearchInput
-              type="text"
-              placeholder="주제, 스터디머 등으로 검색해보세요"
-            ></SearchInput>
-            <SearchIcon />
-          </div>
-          <Filter>
-            {filterOpt.map((opt, idx) => (
-              <option value={opt} key={idx}>
-                {opt}
-              </option>
-            ))}
-          </Filter>
-        </SearchSection>
-        {contents.length ? (
-          isLoading ? (
-            <Loading wsize={50} hsize={50} />
-          ) : (
-            <MainContents contents={contents} />
-          )
-        ) : isLoading ? (
+      <Slider />
+      <SearchSection>
+        <div className="input-box">
+          <SearchInput
+            type="text"
+            placeholder="주제, 스터디머 등으로 검색해보세요"
+            onChange={(e) => searchItem(e.target.value)}
+          ></SearchInput>
+          <SearchIcon />
+        </div>
+        <Filter
+          onChange={(e) => {
+            setSelectedFilterOpt(e.target.value);
+          }}
+        >
+          {filterOpt.map((opt, idx) => (
+            <option value={opt} key={idx}>
+              {opt}
+            </option>
+          ))}
+        </Filter>
+      </SearchSection>
+      {contents.length ? (
+        isLoading ? (
           <Loading wsize={50} hsize={50} />
         ) : (
-          <NotContents>
-            <h2>현재 스터디밍이 없습니다...</h2>
-            <img src={empty} alt="" style={{ width: "100px" }} />
-          </NotContents>
-        )}
-      </section>
+          <MainContents contents={searchItems} />
+        )
+      ) : isLoading ? (
+        <Loading wsize={50} hsize={50} />
+      ) : (
+        <NotContents>
+          <h2>현재 스터디밍이 없습니다...</h2>
+          <img src={empty} alt="" style={{ width: "100px" }} />
+        </NotContents>
+      )}
       <TopBtn />
     </>
   );
