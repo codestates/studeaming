15f1b5c198185add24689f91375679d04ec1df9d
yeinMOON <yeindianamoon@gmail.com>
2021-12-06 17:24:53 +0900

    feat: Add speech bubble to badge

--- client/src/components/Badge.jsx
+++ client/src/components/Badge.jsx
@@ -5,18 +5,17 @@ const Container = styled.div`
   height: 100%;
   gap: 0.8rem;
   border-radius: 10px;
-  /* background-color: var(--color-main-50); */
   display: grid;
   grid-template-rows: repeat(3, 1fr);
   grid-template-columns: repeat(3, 1fr);
 
-  @media screen and (max-width: 768px) {
+  /* @media screen and (max-width: 768px) {
     display: flex;
     justify-content: flex-start;
+    width: fit-content;
     height: fit-content;
     gap: 2px;
-    width: fit-content;
-  }
+  } */
 `;
 
 const BadgeBox = styled.div`
@@ -27,7 +26,58 @@ const BadgeBox = styled.div`
   background-color: ${(props) =>
     props.isGet ? `var(--color-${props.color})` : "var(--color-gray-bg)"};
   border-radius: 30%;
-  font-size: 1.4rem;
+
+  .bubble {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    position: absolute;
+    width: 150px;
+    padding: 0.6rem 0.8rem;
+    bottom: 100%;
+    left: 50%;
+    margin-left: -60px;
+    margin-bottom: 10px;
+    visibility: hidden;
+    border-radius: 10px;
+    background-color: var(--color-main-25);
+    z-index: 100;
+
+    .name {
+      display: inline-block;
+      font-size: 0.8rem;
+      font-weight: 600;
+      color: var(--color-main-100);
+      word-break: normal;
+    }
+
+    .description {
+      display: inline-block;
+      font-size: 0.8rem;
+      color: var(--color-black-50);
+      margin-top: 3px;
+    }
+
+    ::after {
+      content: "";
+      position: absolute;
+      top: 100%;
+      left: 35%;
+      margin-left: -8px;
+      border-width: 8px;
+      border-style: solid;
+      border-color: var(--color-main-25) transparent transparent transparent;
+    }
+  }
+  .bubble-only-title {
+    width: fit-content;
+  }
+
+  :hover {
+    .bubble {
+      visibility: visible;
+    }
+  }
 
   .not_achieved {
     position: absolute;
@@ -39,13 +89,17 @@ const BadgeBox = styled.div`
     border-radius: 30%;
   }
 
-  @media screen and (max-width: 768px) {
-    width: 40px;
-    height: 40px;
+  .emoticon {
+    font-size: 22px;
   }
+
+  /* @media screen and (max-width: 768px) {
+    width: 30px;
+    height: 30px;
+  } */
 `;
 
-function Badge({ badges }) {
+function Badge({ badges, isOthersProfile }) {
   return (
     <Container>
       {badges.map((badge, idx) => {
@@ -53,7 +107,13 @@ function Badge({ badges }) {
         return (
           <BadgeBox key={idx} color={color} isGet={isGet}>
             {isGet ? null : <div className="not_achieved"></div>}
-            {emoticon}
+            <div className={`bubble ${isOthersProfile && "bubble-only-title"}`}>
+              <span className="name">{name}</span>
+              {!isOthersProfile && (
+                <span className="description">{description}</span>
+              )}
+            </div>
+            <span className="emoticon">{emoticon}</span>
           </BadgeBox>
         );
       })}
