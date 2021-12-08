d4b68c84b0191b5567ef4fd37c4f0c352739587f
yeinMOON <yeindianamoon@gmail.com>
2021-12-08 17:21:35 +0900

    feat: Add ASMR audio info asset

---
+++ client/src/assets/sound/index.js
@@ -0,0 +1,33 @@
+import writingImg from "../images/writing.jpeg";
+import waveImg from "../images/wave.jpeg";
+import nightImg from "../images/night.jpeg";
+import fireImg from "../images/fire.jpeg";
+
+const sound = [
+  {
+    keyword: "fire",
+    title: "따듯한 모닥불 소리",
+    img: fireImg,
+    url: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3",
+  },
+  {
+    keyword: "wave",
+    title: "시원한 파도 소리",
+    img: waveImg,
+    url: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3",
+  },
+  {
+    keyword: "night",
+    title: "밤 풍경 소리",
+    img: nightImg,
+    url: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3",
+  },
+  {
+    keyword: "writing",
+    title: "사각사각 필기 소리",
+    img: writingImg,
+    url: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3",
+  },
+];
+
+export default sound;
