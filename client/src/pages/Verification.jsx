3d290f49edde3b4dec28df0569a135f08a2d5d09
yeinMOON <yeindianamoon@gmail.com>
2021-12-08 10:05:22 +0900

    feat: Add notification

--- client/src/pages/Verification.jsx
+++ client/src/pages/Verification.jsx
@@ -1,26 +1,37 @@
 import { useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 import { useNavigate } from "react-router";
-import { signinModalOpen } from "../store/actions";
+import { notification } from "antd";
+import "antd/dist/antd.css";
 import authAPI from "../api/auth";
 
 function Verification() {
   const { isSigninOpen } = useSelector((state) => state.modalReducer);
-  const dispatch = useDispatch();
   const navigate = useNavigate();
 
   const getVerificationCode = () => {
     const url = new URL(window.location.href);
     const authorizationCode = url.searchParams.get("code");
-    console.log(authorizationCode);
     authAPI
       .verification(authorizationCode)
       .then(() => {
-        // notify("이메일 인증이 완료되었습니다. 로그인 후 사용해주세요") 추가
-        dispatch(signinModalOpen(true));
+        notification.open({
+          message: (
+            <div style={{ fontSize: "1rem" }}>
+              이메일 인증이 완료되었습니다.
+            </div>
+          ),
+          description: (
+            <div style={{ fontSize: "0.8rem" }}>로그인 후 사용해주세요.</div>
+          ),
+        });
       })
       .catch(() => {
-        // notify("이메일 인증에 실패했습니다.") 추가
+        notification.open({
+          message: (
+            <div style={{ fontSize: "1rem" }}>이메일 인증에 실패했습니다.</div>
+          ),
+        });
       });
   };
 
