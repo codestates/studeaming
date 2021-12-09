dfc7539a7520738ee2b2ec9dbbacd53e3ceb50bd
Gyosic <zpzptls@naver.com>
2021-12-10 02:44:15 +0900

    feat: Add LandingSection3, 4

--- client/src/components/Landing/LandingSection3.jsx
+++ client/src/components/Landing/LandingSection3.jsx
@@ -3,12 +3,99 @@ import styled from "styled-components";
 const Container = styled.div`
   width: 100%;
   height: calc(100vh - 69.28px);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  overflow: hidden;
+  border: 1px solid;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: start;
+
+  @media screen and (max-width: 900px) {
+    flex-direction: column;
+  }
+`;
+
+const Description = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: end;
+  padding-right: 40px;
+  min-width: 352px;
+
+  @media screen and (max-width: 900px) {
+    align-items: start;
+    padding: 0;
+  }
+
+  @media screen and (max-width: 600px) {
+    min-width: 330px;
+  }
+
+  > h2 {
+    font-size: 1.6rem;
+  }
+
+  > span {
+    margin-bottom: 20px;
+    font-size: 1.15rem;
+    text-align: end;
+
+    @media screen and (max-width: 900px) {
+      text-align: start;
+    }
+  }
+`;
+
+const CalenderImg = styled.div`
+  width: 500px; //600px or 800px
+  height: 300px; //375px or 500px
+  position: relative;
+  border: 1px solid;
+
+  @media screen and (max-width: 600px) {
+    width: 330px;
+    height: 198px;
+  }
+`;
+
+const DailyImg = styled.div`
+  position: absolute;
+  width: 500px; //600px or 800px
+  height: 300px; //375px or 500px
+  opacity: 0;
+
+  @media screen and (max-width: 600px) {
+    width: 330px;
+    height: 198px;
+  }
 `;
 
 function LandingSection3() {
   return (
     <Container>
-      <div></div>
+      <Wrapper>
+        <Description>
+          <h2>학습 기록 관리</h2>
+          <span>미션을 성공하고 훈장을 모아보세요</span>
+          <span>
+            월별 공부시간을 한 눈에 확인해보세요
+            <br />
+            공부 시간이 많아질수록 포도 농장이 풍성해집니다
+          </span>
+          <span>
+            매일의 공부 기록을 확인할 수 있습니다
+            <br />
+            그날의 코멘트도 남겨보세요
+          </span>
+        </Description>
+        <CalenderImg>
+          <DailyImg />
+        </CalenderImg>
+      </Wrapper>
     </Container>
   );
 }
--- client/src/components/Landing/LandingSection4.jsx
+++ client/src/components/Landing/LandingSection4.jsx
@@ -1,14 +1,164 @@
+import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
+import Button from "../Button";
 
 const Container = styled.div`
   width: 100%;
   height: calc(100vh - 69.28px);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  overflow: hidden;
+  border: 1px solid;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: start;
+  position: relative;
+
+  > .button_section {
+    width: 10rem;
+    position: absolute;
+    bottom: 0px;
+    right: 0px;
+
+    @media screen and (max-width: 600px) {
+      right: 25%;
+      bottom: -70px;
+    }
+  }
+
+  @media screen and (max-width: 900px) {
+    flex-direction: column;
+    height: 650px;
+  }
+`;
+
+const SideLogImg = styled.div`
+  width: 300px;
+  height: 550px;
+  border: 1px solid;
+  position: relative;
+
+  @media screen and (max-width: 900px) {
+    width: 250px;
+    height: 450px;
+    position: absolute;
+    left: 60px;
+    bottom: 30px;
+  }
+
+  @media screen and (max-width: 600px) {
+    width: 250px;
+    height: 450px;
+    position: absolute;
+    left: 0px;
+    bottom: 0px;
+  }
+`;
+
+const ToggleImg1 = styled.div`
+  width: 80px;
+  height: 80px;
+  border: 1px solid;
+  border-radius: 0.5rem;
+  position: absolute;
+  right: 15%;
+  bottom: 30%;
+
+  @media screen and (max-width: 900px) {
+    width: 70px;
+    height: 70px;
+  }
+
+  @media screen and (max-width: 600px) {
+    right: 35%;
+  }
+`;
+
+const ToggleImg2 = styled.div`
+  width: 80px;
+  height: 80px;
+  border: 1px solid;
+  border-radius: 0.5rem;
+  position: absolute;
+  right: -20%;
+  bottom: 20%;
+
+  @media screen and (max-width: 900px) {
+    width: 70px;
+    height: 70px;
+  }
+
+  @media screen and (max-width: 600px) {
+    right: 3%;
+  }
+`;
+
+const Description = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: start;
+  padding-left: 40px;
+
+  @media screen and (max-width: 900px) {
+    order: -1;
+    padding: 0;
+    margin-left: 300px;
+    align-items: end;
+  }
+
+  @media screen and (max-width: 600px) {
+    margin-left: 0px;
+  }
+
+  > h2 {
+    font-size: 1.6rem;
+    margin-bottom: 25px;
+  }
+
+  > div {
+    display: flex;
+    flex-direction: column;
+    align-items: start;
+    margin-bottom: 30px;
+    font-size: 1.15rem;
+
+    @media screen and (max-width: 900px) {
+      align-items: end;
+    }
+  }
 `;
 
 function LandingSection4() {
+  const navigate = useNavigate();
+
+  const buttonHandler = () => {
+    navigate("/home");
+  };
+
   return (
     <Container>
-      <div></div>
+      <Wrapper>
+        <SideLogImg>
+          <ToggleImg1 />
+          <ToggleImg2 />
+        </SideLogImg>
+        <Description>
+          <h2>공부 습관</h2>
+          <div>
+            <span>더 이상 다이어리를 꺼내 메모하지 마세요</span>
+          </div>
+          <div>
+            <span>클릭 한번으로 공부 시간을 간편하게 기록하고</span>
+            <span>하루의 공부 패턴을 확인할 수 있습니다</span>
+          </div>
+        </Description>
+        <div className="button_section">
+          <Button message="시작하기" clickEvent={buttonHandler} />
+        </div>
+      </Wrapper>
     </Container>
   );
 }
