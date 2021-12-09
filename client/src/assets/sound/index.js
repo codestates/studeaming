2e381f334f0c1280f8da46eb5729de20d55c7aa3
yeinMOON <yeindianamoon@gmail.com>
2021-12-09 13:57:46 +0900

    fix: Move sound mp3 to public to prevent repeated file req

--- client/src/assets/sound/index.js
+++ client/src/assets/sound/index.js
@@ -2,35 +2,31 @@ import fireImg from "../images/sound_img_fire.jpeg";
 import streamImg from "../images/sound_img_stream.jpg";
 import nightImg from "../images/sound_img_night.jpeg";
 import rainImg from "../images/sound_img_rain.jpg";
-import fireAudio from "./fire.mp3";
-import streamAudio from "./stream.mp3";
-import nightAudio from "./night.mp3";
-import rainAudio from "./rain.mp3";
 
 const sound = [
   {
     keyword: "fire",
     title: "따듯한 모닥불 소리",
     img: fireImg,
-    url: fireAudio,
+    url: `${process.env.PUBLIC_URL}/assets/sound/fire.mp3`,
   },
   {
     keyword: "stream",
     title: "시원한 시냇물 소리",
     img: streamImg,
-    url: streamAudio,
+    url: `${process.env.PUBLIC_URL}/assets/sound/stream.mp3`,
   },
   {
     keyword: "night",
     title: "밤 풍경 소리",
     img: nightImg,
-    url: nightAudio,
+    url: `${process.env.PUBLIC_URL}/assets/sound/night.mp3`,
   },
   {
     keyword: "rain",
     title: "추적추적 빗소리",
     img: rainImg,
-    url: rainAudio,
+    url: `${process.env.PUBLIC_URL}/assets/sound/rain.mp3`,
   },
 ];
 
