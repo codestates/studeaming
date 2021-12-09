5ac268652398009dd9b524b9db410e6de42767ad
yeinMOON <yeindianamoon@gmail.com>
2021-12-09 10:15:42 +0900

    feat: Add ASMR mp3 files

--- client/src/assets/sound/index.js
+++ client/src/assets/sound/index.js
@@ -1,32 +1,36 @@
-import writingImg from "../images/writing.jpeg";
-import waveImg from "../images/wave.jpeg";
-import nightImg from "../images/night.jpeg";
-import fireImg from "../images/fire.jpeg";
+import fireImg from "../images/sound_img_fire.jpeg";
+import streamImg from "../images/sound_img_stream.jpg";
+import nightImg from "../images/sound_img_night.jpeg";
+import rainImg from "../images/sound_img_rain.jpg";
+import fireAudio from "./fire.mp3";
+import streamAudio from "./stream.mp3";
+import nightAudio from "./night.mp3";
+import rainAudio from "./rain.mp3";
 
 const sound = [
   {
     keyword: "fire",
     title: "따듯한 모닥불 소리",
     img: fireImg,
-    url: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3",
+    url: fireAudio,
   },
   {
-    keyword: "wave",
-    title: "시원한 파도 소리",
-    img: waveImg,
-    url: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3",
+    keyword: "stream",
+    title: "시원한 시냇물 소리",
+    img: streamImg,
+    url: streamAudio,
   },
   {
     keyword: "night",
     title: "밤 풍경 소리",
     img: nightImg,
-    url: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3",
+    url: nightAudio,
   },
   {
-    keyword: "writing",
-    title: "사각사각 필기 소리",
-    img: writingImg,
-    url: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3",
+    keyword: "rain",
+    title: "추적추적 빗소리",
+    img: rainImg,
+    url: rainAudio,
   },
 ];
 
