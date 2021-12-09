dd53c87f372c6c1e3db753863698dd909ca9a791
yeinMOON <yeindianamoon@gmail.com>
2021-12-09 21:52:49 +0900

    feat: Initial setting for landing page

---
+++ client/src/components/Landing/LandingSection3.jsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 100%;
+  height: calc(100vh - 69.28px);
+`;
+
+function LandingSection3() {
+  return (
+    <Container>
+      <div></div>
+    </Container>
+  );
+}
+
+export default LandingSection3;
