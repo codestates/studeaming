b698c0e1d2197a78c2e70786a536ce4d469562ff
Gyosic <zpzptls@naver.com>
2021-12-11 01:24:20 +0900

    design: Add animation for LandingSection4

--- client/src/components/Landing/LandingSection4.jsx
+++ client/src/components/Landing/LandingSection4.jsx
@@ -1,6 +1,9 @@
-import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 import styled from "styled-components";
-import Button from "../Button";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { ToggleSection, Name, ToggleBackground, Circle } from "../ToggleBox";
+import StartBtn from "./StartBtn";
 
 const Container = styled.div`
   width: 100%;
@@ -9,7 +12,6 @@ const Container = styled.div`
   align-items: center;
   justify-content: center;
   overflow: hidden;
-  border: 1px solid;
 `;
 
 const Wrapper = styled.div`
@@ -18,14 +20,12 @@ const Wrapper = styled.div`
   position: relative;
 
   > .button_section {
-    width: 10rem;
     position: absolute;
     bottom: 0px;
     right: 0px;
 
     @media screen and (max-width: 600px) {
-      right: 25%;
-      bottom: -70px;
+      display: none;
     }
   }
 
@@ -35,10 +35,9 @@ const Wrapper = styled.div`
   }
 `;
 
-const SideLogImg = styled.div`
+const ImageContainer = styled.div`
   width: 300px;
   height: 550px;
-  border: 1px solid;
   position: relative;
 
   @media screen and (max-width: 900px) {
@@ -50,22 +49,39 @@ const SideLogImg = styled.div`
   }
 
   @media screen and (max-width: 600px) {
-    width: 250px;
-    height: 450px;
-    position: absolute;
+    position: relative;
+    align-self: center;
     left: 0px;
     bottom: 0px;
+    margin-top: 20px;
   }
 `;
 
-const ToggleImg1 = styled.div`
-  width: 80px;
-  height: 80px;
-  border: 1px solid;
-  border-radius: 0.5rem;
+const SideLogImg = styled.div`
+  width: 100%;
+  height: 100%;
+  /* border: 1px solid; */
+  box-shadow: 0 0 10px var(--color-black-25);
+  position: relative;
+  background-image: url("/assets/images/logchartimg.png");
+  background-size: contain;
+  border-radius: 1.5rem;
+
+  > #moving-log {
+    position: absolute;
+    top: 40.6%;
+    left: 16%;
+    width: 0;
+    height: 2.8%;
+    background-color: var(--color-blue);
+  }
+`;
+
+const ToggleImg1 = styled(ToggleSection)`
   position: absolute;
   right: 15%;
   bottom: 30%;
+  box-shadow: 0 0 10px var(--color-black-25);
 
   @media screen and (max-width: 900px) {
     width: 70px;
@@ -77,14 +93,11 @@ const ToggleImg1 = styled.div`
   }
 `;
 
-const ToggleImg2 = styled.div`
-  width: 80px;
-  height: 80px;
-  border: 1px solid;
-  border-radius: 0.5rem;
+const ToggleImg2 = styled(ToggleSection)`
   position: absolute;
   right: -20%;
   bottom: 20%;
+  box-shadow: 0 0 10px var(--color-black-25);
 
   @media screen and (max-width: 900px) {
     width: 70px;
@@ -94,6 +107,10 @@ const ToggleImg2 = styled.div`
   @media screen and (max-width: 600px) {
     right: 3%;
   }
+`;
+
+const ToggleBack = styled(ToggleBackground)`
+  background: var(--color-gray-bg-100);
 `;
 
 const Description = styled.section`
@@ -116,47 +133,147 @@ const Description = styled.section`
   > h2 {
     font-size: 1.6rem;
     margin-bottom: 25px;
+
+    @media screen and (max-width: 600px) {
+      margin-bottom: 10px;
+    }
   }
 
-  > div {
-    display: flex;
-    flex-direction: column;
-    align-items: start;
+  > span {
+    text-align: start;
     margin-bottom: 30px;
     font-size: 1.15rem;
+    opacity: 0;
 
     @media screen and (max-width: 900px) {
-      align-items: end;
+      text-align: end;
+    }
+
+    @media screen and (max-width: 600px) {
+      margin-bottom: 10px;
     }
   }
 `;
 
 function LandingSection4() {
-  const navigate = useNavigate();
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
 
-  const buttonHandler = () => {
-    navigate("/home");
-  };
+    gsap.from("#toggle-image1", {
+      scrollTrigger: {
+        trigger: "#section4-container",
+        toggleActions: "restart none none none",
+        start: "top center",
+      },
+      x: -50,
+      duration: 1,
+      opacity: 0,
+      ease: "power0",
+    });
+
+    gsap.from("#toggle-image2", {
+      scrollTrigger: {
+        trigger: "#section4-container",
+        toggleActions: "restart none none none",
+        start: "top center",
+      },
+      x: 50,
+      duration: 1,
+      delay: 0.5,
+      opacity: 0,
+      ease: "power0",
+    });
+
+    gsap.to("#section4-description-1", {
+      scrollTrigger: {
+        trigger: "#section4-container",
+        toggleActions: "restart none none none",
+        start: "top center",
+      },
+      duration: 1,
+      opacity: 1,
+      ease: "power1",
+    });
+
+    gsap.to("#section4-description-2", {
+      scrollTrigger: {
+        trigger: "#section4-container",
+        toggleActions: "restart none none none",
+        start: "top center",
+      },
+      duration: 1,
+      delay: 0.5,
+      opacity: 1,
+      ease: "power1",
+    });
+
+    gsap.to("#moving-circle", {
+      scrollTrigger: {
+        trigger: "#section4-container",
+        toggleActions: "restart none none none",
+        start: "top center",
+      },
+      delay: 2,
+      duration: 0.5,
+      x: 20,
+    });
+
+    gsap.to("#toggle-back", {
+      scrollTrigger: {
+        trigger: "#section4-container",
+        toggleActions: "restart none none none",
+        start: "top center",
+      },
+      delay: 2,
+      duration: 0.5,
+      backgroundColor: "#E2ECFC",
+    });
+
+    gsap.to("#moving-log", {
+      scrollTrigger: {
+        trigger: "#section4-container",
+        toggleActions: "restart none none none",
+        start: "top center",
+      },
+      delay: 2.5,
+      duration: 1,
+      width: 150,
+    });
+  }, []);
 
   return (
     <Container>
-      <Wrapper>
-        <SideLogImg>
-          <ToggleImg1 />
-          <ToggleImg2 />
-        </SideLogImg>
+      <Wrapper id="section4-container">
+        <ImageContainer>
+          <SideLogImg>
+            <div id="moving-log" />
+          </SideLogImg>
+          <ToggleImg1 id="toggle-image1">
+            <Name>기하와 벡터</Name>
+            <ToggleBack id="toggle-back" color="blue">
+              <Circle id="moving-circle" />
+            </ToggleBack>
+          </ToggleImg1>
+          <ToggleImg2 id="toggle-image2">
+            <Name>미분과 적분</Name>
+            <ToggleBackground color="red">
+              <Circle />
+            </ToggleBackground>
+          </ToggleImg2>
+        </ImageContainer>
         <Description>
           <h2>공부 습관</h2>
-          <div>
-            <span>더 이상 다이어리를 꺼내 메모하지 마세요</span>
-          </div>
-          <div>
-            <span>클릭 한번으로 공부 시간을 간편하게 기록하고</span>
-            <span>하루의 공부 패턴을 확인할 수 있습니다</span>
-          </div>
+          <span id="section4-description-1">
+            더 이상 다이어리를 꺼내 메모하지 마세요
+          </span>
+          <span id="section4-description-2">
+            클릭 한번으로 공부 시간을 간편하게 기록하고
+            <br />
+            하루의 공부 패턴을 확인할 수 있습니다
+          </span>
         </Description>
         <div className="button_section">
-          <Button message="시작하기" clickEvent={buttonHandler} />
+          <StartBtn />
         </div>
       </Wrapper>
     </Container>
