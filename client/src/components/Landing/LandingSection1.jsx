00ace131a9ec5a91fcf7daf500c28a0eef0fd896
yeinMOON <yeindianamoon@gmail.com>
2021-12-10 21:08:00 +0900

    feat: Add button animation

--- client/src/components/Landing/LandingSection1.jsx
+++ client/src/components/Landing/LandingSection1.jsx
@@ -12,10 +12,7 @@ const Container = styled.div`
   justify-content: center;
 
   #start-btn-container {
-    width: 10rem;
-    padding: 2px;
-    border: 1px solid var(--color-main-50);
-    border-radius: 10px;
+    margin-top: 1rem;
   }
 
   #scroll-line {
@@ -38,6 +35,66 @@ const WelcomeMsg = styled.h1`
   text-align: center;
 `;
 
+const StartBtn = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+  width: 100px;
+  height: 100px;
+  border-radius: 50%;
+  background-color: var(--color-main-100);
+  color: #fff;
+  text-align: center;
+  line-height: 80px;
+  font-size: 1rem;
+
+  ::before,
+  ::after {
+    content: "";
+    display: block;
+    border: 1px solid var(--color-main-100);
+    position: absolute;
+    left: -20px;
+    right: -20px;
+    top: -20px;
+    bottom: -20px;
+    border-radius: 50%;
+    animation: animate 1.5s linear infinite;
+    backface-visibility: hidden;
+  }
+
+  ::after {
+    animation-delay: 0.5s;
+  }
+
+  :hover {
+    cursor: pointer;
+    ::before,
+    ::after {
+      animation: animate 0.75s linear infinite;
+    }
+
+    ::after {
+      animation-delay: 0.25s;
+    }
+  }
+
+  @keyframes animate {
+    0% {
+      transform: scale(0.5);
+      opacity: 0;
+    }
+    50% {
+      opacity: 1;
+    }
+    100% {
+      transform: scale(1.1);
+      opacity: 0;
+    }
+  }
+`;
+
 function LandingSection1() {
   const navigate = useNavigate();
 
@@ -53,7 +110,7 @@ function LandingSection1() {
         나만의 기록을 남기세요
       </WelcomeMsg>
       <div id="start-btn-container">
-        <Button clickEvent={startBtnHandler} message="시작하기" />
+        <StartBtn>시작하기</StartBtn>
       </div>
       <div id="scroll-line"></div>
       <div id="scroll-text">스크롤</div>
