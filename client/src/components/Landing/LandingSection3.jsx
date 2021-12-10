679c48e1757de371f4d16b1d43cc81ec12e09d6a
Gyosic <zpzptls@naver.com>
2021-12-10 21:11:18 +0900

    design: Add animation LandingSection3

--- client/src/components/Landing/LandingSection3.jsx
+++ client/src/components/Landing/LandingSection3.jsx
@@ -1,3 +1,6 @@
+import { useEffect } from "react";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 import styled from "styled-components";
 
 const Container = styled.div`
@@ -43,6 +46,7 @@ const Description = styled.section`
     margin-bottom: 20px;
     font-size: 1.15rem;
     text-align: end;
+    opacity: 0;
 
     @media screen and (max-width: 900px) {
       text-align: start;
@@ -50,10 +54,23 @@ const Description = styled.section`
   }
 `;
 
-const CalenderImg = styled.div`
+const ImageContainer = styled.div`
   width: 500px; //600px or 800px
-  height: 300px; //375px or 500px
+  height: 300px;
   position: relative;
+
+  @media screen and (max-width: 600px) {
+    width: 330px;
+    height: 198px;
+  }
+`;
+
+const CalenderImg = styled.div`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
   border: 1px solid;
 
   @media screen and (max-width: 600px) {
@@ -64,9 +81,12 @@ const CalenderImg = styled.div`
 
 const DailyImg = styled.div`
   position: absolute;
-  width: 500px; //600px or 800px
-  height: 300px; //375px or 500px
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
   opacity: 0;
+  background-color: lightgrey;
 
   @media screen and (max-width: 600px) {
     width: 330px;
@@ -75,26 +95,61 @@ const DailyImg = styled.div`
 `;
 
 function LandingSection3() {
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: "#section3-container",
+        pin: true,
+        scrub: true,
+        start: "top 69.28px",
+        end: "+=3000",
+      },
+    });
+    tl.from("#section3-image", {
+      scale: 2,
+      x: 400,
+    })
+      .to("#section3-description-2", {
+        opacity: 1,
+      })
+      .to("#section3-modal-image, #section3-description-3", {
+        opacity: 1,
+      });
+
+    gsap.to("#section3-description-1", {
+      scrollTrigger: {
+        trigger: "#section3-container",
+        toggleActions: "restart none none none",
+        start: "top center",
+      },
+      opacity: 1,
+      duration: 0.5,
+    });
+  });
   return (
-    <Container>
+    <Container id="section3-container">
       <Wrapper>
         <Description>
           <h2>학습 기록 관리</h2>
-          <span>미션을 성공하고 훈장을 모아보세요</span>
-          <span>
+          <span id="section3-description-1">
+            미션을 성공하고 훈장을 모아보세요
+          </span>
+          <span id="section3-description-2">
             월별 공부시간을 한 눈에 확인해보세요
             <br />
             공부 시간이 많아질수록 포도 농장이 풍성해집니다
           </span>
-          <span>
+          <span id="section3-description-3">
             매일의 공부 기록을 확인할 수 있습니다
             <br />
             그날의 코멘트도 남겨보세요
           </span>
         </Description>
-        <CalenderImg>
-          <DailyImg />
-        </CalenderImg>
+        <ImageContainer>
+          <CalenderImg id="section3-image" />
+          <DailyImg id="section3-modal-image" />
+        </ImageContainer>
       </Wrapper>
     </Container>
   );
