cbfe83d25dd8b2adc77d77134d38ba7e8294ac94
Soomin Na <soominnna@gmail.com>
2021-12-11 23:37:40 +0900

    refactor: Remove unnecessity

--- server/app.js
+++ server/app.js
@@ -1,15 +1,11 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
-const fs = require("fs");
-const https = require("https");
 const cors = require("cors");
 const cron = require("node-cron");
 const swaggerUi = require("swagger-ui-express");
 const swaggerJsdoc = require("swagger-jsdoc");
 const { Server } = require("socket.io");
-
-const studyRoom = require("./controller/studyRoom/index");
 const authRouter = require("./routes/auth");
 const userRouter = require("./routes/user");
 const studyToggleRouter = require("./routes/studyToggle");
@@ -17,24 +13,26 @@ const studylogRouter = require("./routes/studylog");
 const verifyRouter = require("./routes/verify");
 const studyroomRouter = require("./routes/studyroom");
 const cronJob = require("./controller/functions/cronJob");
+const studyRoom = require("./controller/studyRoom/index");
 const options = require("./swagger.js");
 
 const apiSpec = swaggerJsdoc(options);
 require("dotenv").config();
 
 const app = express();
+const corsOpt = {
+  origin: process.env.CLIENT_URL || "http://localhost:3000",
+  credentials: true,
+  samesite: "none",
+  secure: true,
+};
 
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use(
-  cors({
-    origin: process.env.CLIENT_URL || "http://localhost:3000",
-    credentials: true,
-  })
-);
+app.use(cors(corsOpt));
 
 cron.schedule("0 0 * * 1", () => {
   cronJob();
@@ -57,9 +55,6 @@ app.use("/studytoggle", studyToggleRouter);
 app.use("/studyroom", studyroomRouter);
 app.use("/verification", verifyRouter);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(apiSpec));
-//경로 설정
-
-//app.get("/*", (_, res) => res.redirect("/"));
 
 app.use(function (err, req, res, next) {
   res.status(err.status || 404);
@@ -68,36 +63,17 @@ app.use(function (err, req, res, next) {
 
 const PORT = process.env.HTTPS_PORT || 4000;
 
-let server;
+const http = require("http");
 
-if (fs.existsSync("./key.pem") && fs.existsSync("./cert.pem")) {
-  const privateKey = fs.readFileSync(__dirname + "/key.pem", "utf8");
-  const certificate = fs.readFileSync(__dirname + "/cert.pem", "utf8");
-  const credentials = { key: privateKey, cert: certificate };
+const server = http.Server(app).listen(PORT, () => {
+  console.log("Express server listening on port ");
+});
 
-  server = https.createServer(credentials, app);
-  const io = new Server(server);
-  io.on("connection", studyRoom.io);
-  server.listen(PORT, () => console.log("https server runnning"));
-} else {
-  //todo:// https랑 같이 사용할경우 One and only one of the port server or no Server options must be specified
-  //todo:// 해당 에러 발생 , 임시로 에러 해결을 위해 port 4001로 변경, 차후에 https 배포되면 여기는 필요없음
-  // const WebSocket = require("ws").Server;
-  const http = require("http");
-  // todo: http를 따로 설정하고
-  server = http.Server(app).listen(PORT, function () {
-    console.log("Express server listening on port ");
-  });
+const io = new Server(server, {
+  cors: corsOpt,
+  transports: ["websocket"],
+});
 
-  const io = new Server(server, {
-    cors: {
-      origin: process.env.CLIENT_URL || "http://localhost:3000",
-      credentials: true,
-    },
-    transports: ["websocket"],
-  });
-
-  io.on("connection", (socket) => studyRoom.io(socket, io));
-}
+io.on("connection", (socket) => studyRoom.io(socket, io));
 
 module.exports = server;
