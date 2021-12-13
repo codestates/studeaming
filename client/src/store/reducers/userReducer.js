49e08a190ba040f4f0247bc7122cea0d1224ea54
yeinMOON <yeindianamoon@gmail.com>
2021-12-14 00:55:44 +0900

    fix: Remove logout case & Merge to change login state case

--- client/src/store/reducers/userReducer.js
+++ client/src/store/reducers/userReducer.js
@@ -3,7 +3,6 @@ import {
   VERIFY_SOCIAL_LOGINED,
   VERIFY_GUEST_LOGINED,
   GET_USERINFO,
-  LOGOUT,
 } from "../actions/index";
 
 const initState = {
@@ -20,15 +19,14 @@ const initState = {
 const userReducer = (state = initState, action) => {
   switch (action.type) {
     case CHANGE_LOGIN_STATE:
-      return { ...state, isLogin: action.payload };
+      if (!action.payload) return { ...initState };
+      else return { ...state, isLogin: action.payload };
     case VERIFY_SOCIAL_LOGINED:
       return { ...state, isSocialLogined: action.payload };
     case VERIFY_GUEST_LOGINED:
       return { ...state, isGuestLogined: action.payload };
     case GET_USERINFO:
       return { ...state, ...action.payload.data };
-    case LOGOUT:
-      return { ...initState };
     default:
       return state;
   }
