7c06e01ea5015cebb6d97ce246da86f661bfc1f0
Soomin Na <soominnna@gmail.com>
2021-12-14 17:13:50 +0900

    chore: Change thumbnail and uuid in asmrSound page

--- server/seeders/20211206141108-Studyroom.js
+++ server/seeders/20211206141108-Studyroom.js
@@ -7,26 +7,23 @@ module.exports = {
       [
         {
           user_id: 0,
-          uuid: "0",
+          uuid: "fire",
           title: "장작 타는 소리 들으며 공부하기",
-          thumbnail:
-            "https://images.unsplash.com/photo-1543393470-b2c833b98dce?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8ZmlyZXBsYWNlfGVufDB8fDB8fA%3D%3D&w=1000&q=80",
+          thumbnail: "https://i.imgur.com/W2f5uHV.png",
           headCount: 0,
         },
         {
           user_id: 0,
-          uuid: "1",
+          uuid: "stream",
           title: "시냇물 흐르는 소리 들으며 공부하기",
-          thumbnail:
-            "https://images.pexels.com/photos/315998/pexels-photo-315998.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+          thumbnail: "https://i.imgur.com/RebKol8.png",
           headCount: 0,
         },
         {
           user_id: 0,
-          uuid: "2",
+          uuid: "night",
           title: "밤 풍경소리 들으며 공부하기",
-          thumbnail:
-            "https://images.pexels.com/photos/631477/pexels-photo-631477.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+          thumbnail: "https://i.imgur.com/nw0Om7O.png",
           headCount: 0,
         },
       ],
