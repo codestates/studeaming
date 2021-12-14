1e8664f2bc737ead914a20f9e0ac482ddd243925
yeinMOON <yeindianamoon@gmail.com>
2021-12-15 02:30:12 +0900

    feat: Add calendar - graph tab

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -7,7 +7,7 @@ const Container = styled.div`
   width: 300px;
   height: calc(100vh - 80px);
   background-color: var(--color-main-0);
-  display: flex;
+  display: inline-flex;
   flex-direction: column;
   justify-content: column;
   align-items: center;
@@ -39,6 +39,7 @@ const Container = styled.div`
     width: 100vw;
     min-width: 370px;
     height: fit-content;
+    order: 2;
   }
 `;
 
@@ -78,17 +79,63 @@ const Following = styled.ul`
 const BadgeContainer = styled.div`
   width: 220px;
   height: 220px;
+`;
+
+const TabContainer = styled.div`
+  width: 50px;
 
   @media screen and (max-width: 768px) {
-    /* width: 100%;
-    height: fit-content; */
-    /* overflow: scroll; */
+    display: flex;
+    align-items: flex-end;
+    order: 1;
+    margin-left: 20px;
+  }
 
-    /* hide scroll bar */
-    -ms-overflow-style: none; /* IE and Edge */
-    scrollbar-width: none; /* Firefox */
-    ::-webkit-scrollbar {
-      display: none; /* Chrome, Safari, Opera*/
+  span {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 120px;
+    height: 40px;
+    margin-left: 40px;
+    background-color: var(--color-main-0);
+    border-radius: 8px 8px 0 0;
+    border-top: 0.6px solid var(--color-gray-bg-100);
+    border-right: 3px solid var(--color-gray-bg-100);
+    font-family: "Poppins-Regular";
+    cursor: pointer;
+    -ms-transform: rotate(90deg); /* IE 9 */
+    -webkit-transform: rotate(90deg); /* Chrome, Safari, Opera */
+    transform: rotate(90deg);
+    transform-origin: 0 0;
+
+    @media screen and (max-width: 768px) {
+      padding: 10px 20px;
+      margin-left: 0px;
+      -ms-transform: none; /* IE 9 */
+      -webkit-transform: none; /* Chrome, Safari, Opera */
+      transform: none;
+    }
+  }
+
+  .statistics-tab {
+    margin-top: ${(props) =>
+      props.selectedTab === "calendar" ? "70px" : "80px"};
+    @media screen and (max-width: 768px) {
+      margin-top: 0;
+    }
+  }
+
+  .selected {
+    background-color: var(--color-main-50);
+    border-top: 0.6px solid var(--color-gray-bg-100);
+    border-right: 3px solid var(--color-gray-bg-100);
+    height: 50px;
+    margin-left: 50px;
+    border-bottom: none;
+
+    @media screen and (max-width: 768px) {
+      margin-left: 0px;
     }
   }
 `;
@@ -100,52 +147,74 @@ function Sidebar({
   signoutHandler,
   editPwdHandler,
   withdrawalHandler,
+  selectedTab,
+  tabHandler,
 }) {
   const { isSocialLogined, isGuestLogined } = useSelector(
     ({ userReducer }) => userReducer
   );
 
   return (
-    <Container>
-      <section id="following" className="sidebar_section">
-        <SectionTitle>즐겨찾는 스터디머</SectionTitle>
-        <Following>
-          {following.map((studeamer, idx) => {
-            return (
-              <li
-                key={idx}
-                onClick={() => openFollowingProfile(studeamer.username)}
-              >
-                <img
-                  src={studeamer.profileImg || defaultImg}
-                  alt="profile_image"
-                />
-                <span className="following_name">{studeamer.username}</span>
-              </li>
-            );
-          })}
-        </Following>
-      </section>
-      <section id="badges" className="sidebar_section">
-        <SectionTitle>내 훈장</SectionTitle>
-        <BadgeContainer>
-          <Badge badges={badges} />
-        </BadgeContainer>
-      </section>
-      <section id="user_edit" className="sidebar_section">
-        <button id="signout" onClick={signoutHandler}>
-          로그아웃
-        </button>
-        {!(isSocialLogined || isGuestLogined) && (
-          <button id="edit_email" onClick={editPwdHandler}>
-            비밀번호 수정
+    <>
+      <Container>
+        <section id="following" className="sidebar_section">
+          <SectionTitle>즐겨찾는 스터디머</SectionTitle>
+          <Following>
+            {following.map((studeamer, idx) => {
+              return (
+                <li
+                  key={idx}
+                  onClick={() => openFollowingProfile(studeamer.username)}
+                >
+                  <img
+                    src={studeamer.profileImg || defaultImg}
+                    alt="profile_image"
+                  />
+                  <span className="following_name">{studeamer.username}</span>
+                </li>
+              );
+            })}
+          </Following>
+        </section>
+        <section id="badges" className="sidebar_section">
+          <SectionTitle>내 훈장</SectionTitle>
+          <BadgeContainer>
+            <Badge badges={badges} />
+          </BadgeContainer>
+        </section>
+        <section id="user_edit" className="sidebar_section">
+          <button id="signout" onClick={signoutHandler}>
+            로그아웃
           </button>
-        )}
-        <button id="withdrawl" onClick={withdrawalHandler}>
-          회원 탈퇴
-        </button>
-      </section>
-    </Container>
+          {!(isSocialLogined || isGuestLogined) && (
+            <button id="edit_email" onClick={editPwdHandler}>
+              비밀번호 수정
+            </button>
+          )}
+          <button id="withdrawl" onClick={withdrawalHandler}>
+            회원 탈퇴
+          </button>
+        </section>
+      </Container>
+      <TabContainer selectedTab={selectedTab}>
+        <span
+          className={`calendar-tab ${
+            selectedTab === "calendar" ? "selected" : ""
+          }`}
+          onClick={() => tabHandler("calendar")}
+        >
+          calendar
+        </span>
+        <span
+          className={`statistics-tab ${
+            selectedTab === "statistics" ? "selected" : ""
+          }`}
+          onClick={() => tabHandler("statistics")}
+        >
+          graph
+        </span>
+      </TabContainer>
+    </>
   );
 }
 
