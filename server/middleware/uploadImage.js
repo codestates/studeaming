0cba9f765a23f9dfedc1c70826dc48ef2f175038
Soomin Na <soominnna@gmail.com>
2021-12-15 17:19:56 +0900

    chore: Fix typo

--- server/middleware/uploadImage.js
+++ server/middleware/uploadImage.js
@@ -4,7 +4,7 @@ const multer = require("multer");
 module.exports = (req, res, next) => {
   let storage = multer.diskStorage({
     destination: (req, file, cb) => {
-      cb(null, "uploads/profile");
+      cb(null, "uploads");
     },
     filename: (req, file, cb) => {
       cb(null, `${Date.now()}_${file.originalname}`);
