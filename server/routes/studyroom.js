43a34098157bbc591c1507de8572940b324a9554
Soomin Na <soominnna@gmail.com>
2021-12-15 17:15:59 +0900

    feat: Add studyroom post API for thumbnail image upload
    Creating studyroom in database was originally processed in web socket protocol,
    and it is moved to added http method, because sending thumbnail image by ws
    would be large burden to client cpu

--- server/routes/studyroom.js
+++ server/routes/studyroom.js
@@ -1,7 +1,9 @@
 const express = require("express");
-const { getStudyroom } = require("../controller");
+const { studyRoom } = require("../controller");
+const uploadImage = require("../middleware/uploadImage");
 const router = express.Router();
 
-router.get("/", getStudyroom);
+router.get("/", studyRoom.get);
+router.post("/", uploadImage, studyRoom.post);
 
 module.exports = router;
