d824b1483d3a70f97632d9ec5ce26067753cba4c
Gyosic <zpzptls@naver.com>
2021-12-18 14:20:34 +0900

    design: Fix design in active

--- client/src/pages/Viewer.jsx
+++ client/src/pages/Viewer.jsx
@@ -23,6 +23,7 @@ const StyledViewer = styled.section`
 
   @media screen and (max-width: 480px) {
     height: calc(100vh - 60px);
+    padding: 0;
   }
 `;
 
@@ -63,7 +64,7 @@ const Screen = styled.div`
   align-items: center;
   width: 100%;
   max-width: 1280px;
-  min-width: 360px;
+  min-width: 330px;
   height: 100%;
   min-height: 240px;
   position: relative;
@@ -128,14 +129,13 @@ const Mute = styled(ImVolumeMute2)`
 
 const StudeamerInfo = styled.div`
   max-width: 1280px;
-  min-width: 360px;
+  min-width: 330px;
   width: 100%;
   height: 15%;
   min-height: 150px;
   display: flex;
   justify-content: space-between;
   padding: 20px 10px;
-  /* background-color: #f8f8f8; */
 `;
 
 const InfoSection1 = styled.div`
@@ -193,7 +193,7 @@ const ChatSection = styled.section`
 
   @media screen and (max-width: 1000px) {
     width: 100%;
-    min-width: 360px;
+    min-width: 330px;
   }
 `;
 
