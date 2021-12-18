d1973aa987021e32f67c7e3cb6c8be2f9cd40328
Gyosic <zpzptls@naver.com>
2021-12-18 13:03:32 +0900

    chore: Fix design

--- client/src/components/Chat.jsx
+++ client/src/components/Chat.jsx
@@ -26,6 +26,7 @@ const ChatSection = styled.ul`
   flex-direction: column;
   overflow: scroll;
   margin-bottom: 4px;
+  padding: 10px;
 
   ::-webkit-scrollbar {
     display: none;
@@ -242,8 +243,13 @@ function Chat({ socket, viewers, uuid }) {
 
     socket.on("welcome", (user) => {
       const Usernotification = (
-        <div style={{ textAlign: "center" }}>
-          <span style={{ fontSize: "12px", color: "var(--color-black-50)" }}>
+        <div style={{ textAlign: "center", marginBottom: "10px" }}>
+          <span
+            style={{
+              fontSize: "12px",
+              color: "var(--color-black-50)",
+            }}
+          >
             {user.username || "구경꾼"}님이 입장하셨습니다.
           </span>
         </div>
@@ -254,8 +260,13 @@ function Chat({ socket, viewers, uuid }) {
 
     socket.on("leave_room", (_, username) => {
       const Usernotification = (
-        <div style={{ textAlign: "center" }}>
-          <span style={{ fontSize: "12px", color: "var(--color-black-50)" }}>
+        <div style={{ textAlign: "center", marginBottom: "10px" }}>
+          <span
+            style={{
+              fontSize: "12px",
+              color: "var(--color-black-50)",
+            }}
+          >
             {username || "구경꾼"}님이 나가셨습니다.
           </span>
         </div>
